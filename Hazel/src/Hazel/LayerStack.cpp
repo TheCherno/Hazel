9b75670ced984969806658a736c0709801ee3f1d
3x3y3z3t <recette.lemongrass@yahoo.com.vn>
2019-07-02 17:55:21 +0700

    LayerStack only need to search for the layer to be pop in its correspond location (#33)
    * Update LayerStack.cpp
    
    * Updated m_LayerInsert->m_LayerInsertIndex in #33
    
    * Update LayerStack.cpp

--- Hazel/src/Hazel/LayerStack.cpp
+++ Hazel/src/Hazel/LayerStack.cpp
@@ -28,7 +28,7 @@ namespace Hazel {
 
 	void LayerStack::PopLayer(Layer* layer)
 	{
-		auto it = std::find(m_Layers.begin(), m_Layers.end(), layer);
+		auto it = std::find(m_Layers.begin(), m_Layers.begin() + m_LayerInsertIndex, layer);
 		if (it != m_Layers.end())
 		{
 			layer->OnDetach()
@@ -39,7 +39,7 @@ namespace Hazel {
 
 	void LayerStack::PopOverlay(Layer* overlay)
 	{
-		auto it = std::find(m_Layers.begin(), m_Layers.end(), overlay);
+		auto it = std::find(m_Layers.begin() + m_LayerInsertIndex, m_Layers.end(), overlay);
 		if (it != m_Layers.end())
 		{
 			overlay->OnDetach();
