ee049dae533945f87c0fa8b6beef938afea40a2e
LovelySanta <laurens.scheldeman@gmail.com>
2019-07-09 10:58:28 +0200

    Add HZ_DEBUGBREAK() macro

--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -1,42 +1,48 @@
 #pragma once
 
-#ifdef HZ_PLATFORM_WINDOWS
-/*
-#if HZ_DYNAMIC_LINK
-	#ifdef HZ_BUILD_DLL
-		#define HAZEL_API __declspec(dllexport)
-	#else
-		#define HAZEL_API __declspec(dllimport)
-	#endif
-*/
-	#define HAZEL_API
-#elif defined HZ_PLATFORM_LINUX
-/*
-	#ifdef HZ_BUILD_DLL
-		#define HAZEL_API __attribute__((visibility("default")))
+// Dynamic Link Library
+#if defined HZ_PLATFORM_WINDOWS
+	#if defined HZ_DYNAMIC_LINK
+		#if defined HZ_BUILD_DLL
+			#define HAZEL_API __declspec(dllexport)
+		#else
+			#define HAZEL_API __declspec(dllimport)
+		#endif
 	#else
 		#define HAZEL_API
 	#endif
-*/
-	#define HAZEL_API
+
+#elif defined HZ_PLATFORM_LINUX
+	#if defined HZ_DYNAMIC_LINK
+		#if defined HZ_BUILD_DLL
+			#define HAZEL_API __attribute__((visibility("default")))
+		#else
+			#define HAZEL_API
+		#end
+	#else
+		#define HAZEL_API
+	#endif
+
 #else
 	#error Unsupported platform!
-#endif
+#endif // End of Dynamic Link Library
 
+// Debug settings
 #ifdef HZ_DEBUG
+	#if defined HZ_PLATFORM_WINDOWS
+		#define HZ_DEBUGBREAK() __debugbreak()
+	#elif defined HZ_PLATFORM_LINUX
+		#include <signal.h>
+		#define HZ_DEBUGBREAK() raise(SIGTRAP)
+	#endif
+
 	#define HZ_ENABLE_ASSERTS
 #endif
 
+// Asserts
 #ifdef HZ_ENABLE_ASSERTS
-	#if defined HZ_PLATFORM_WINDOWS
-		#define HZ_ASSERT(x, ...) { if(!(x)) { HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
-		#define HZ_CORE_ASSERT(x, ...) { if(!(x)) { HZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
-	#elif defined HZ_PLATFORM_LINUX
-		#include <signal.h>
-		#define HZ_ASSERT(x, ...) { if(!(x)) { HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); raise(SIGTRAP); } }
-		#define HZ_CORE_ASSERT(x, ...) { if(!(x)) { HZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); raise(SIGTRAP); } }
-	#else
-	#endif
+	#define HZ_ASSERT(x, ...) { if(!(x)) { HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); HZ_DEBUGBREAK(); } }
+	#define HZ_CORE_ASSERT(x, ...) { if(!(x)) { HZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); HZ_DEBUGBREAK(); } }
 #else
 	#define HZ_ASSERT(x, ...)
 	#define HZ_CORE_ASSERT(x, ...)
