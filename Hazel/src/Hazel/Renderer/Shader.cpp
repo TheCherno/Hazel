aeeebbef73c4fedee00fa6dd2c3f418e1a34a4d5
3x3y3z3t <>
2019-07-09 16:20:46 +0700

    Update Shader abstraction to compatible with master branch.
    Add UploadUniformMat4 in Shader and its implementation in OpenGLShader.

--- Hazel/src/Hazel/Renderer/Shader.cpp
+++ Hazel/src/Hazel/Renderer/Shader.cpp
@@ -2,7 +2,7 @@
 #include "Shader.h"
 
 #include "Renderer.h"
-#include "Platform\OpenGL\OpenGLShader.h"
+#include "Platform/OpenGL/OpenGLShader.h"
 
 namespace Hazel {
 
--- Hazel/src/Hazel/Renderer/Shader.h
+++ Hazel/src/Hazel/Renderer/Shader.h
@@ -1,16 +1,19 @@
 #pragma once
 
 #include <string>
+#include <glm/glm.hpp>
 
 namespace Hazel {
 
-	class Shader
-	{
-	public:
+    class Shader
+    {
+    public:
         virtual void Bind() const = 0;
-		virtual void Unbind() const = 0;
+        virtual void Unbind() const = 0;
+
+        virtual void UploadUniformMat4(const std::string& name, const glm::mat4& matrix) = 0;
 
         static Shader* Create(const std::string& vertexSrc, const std::string& fragmentSrc);
-	};
+    };
 
 }
