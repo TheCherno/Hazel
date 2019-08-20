134eda579d6da8cf457d475d8aef5ec817781a41
LovelySanta <laurens.scheldeman@gmail.com>
2019-08-20 10:34:17 +0200

    Resolving merge conflicts

--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <memory>
 
 // Dynamic Link Library
 #if defined HZ_PLATFORM_WINDOWS
@@ -60,3 +61,13 @@
 #define BIT(x) (1 << x)
 
 #define HZ_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+
+namespace Hazel {
+
+	template<typename T>
+	using Scope = std::unique_ptr<T>;
+
+	template<typename T>
+	using Ref = std::shared_ptr<T>;
+
+}
