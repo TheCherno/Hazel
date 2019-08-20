f52b813db65cd7275da56003c501914ad1c7515a
jdunlap <jdunlap1974@gmail.com>
2019-08-20 15:30:54 -0500

    Fixed a bug in LayerStack which would occur if passing an overlay or a non-existent layer in to PopLayer. (#93)

--- Hazel/src/Hazel/LayerStack.cpp
+++ Hazel/src/Hazel/LayerStack.cpp
@@ -29,7 +29,7 @@ namespace Hazel {
 	void LayerStack::PopLayer(Layer* layer)
 	{
 		auto it = std::find(m_Layers.begin(), m_Layers.begin() + m_LayerInsertIndex, layer);
-		if (it != m_Layers.end())
+		if (it != m_Layers.begin() + m_LayerInsertIndex)
 		{
 			layer->OnDetach();
 			m_Layers.erase(it);
