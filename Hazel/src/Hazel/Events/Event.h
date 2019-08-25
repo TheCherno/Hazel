572cab2ac6fb13107d242957d81bec30b673e52e
Ryan <stunthound@gmail.com>
2019-08-25 13:10:44 -0400

    Avoid using std::function for performance

--- Hazel/src/Hazel/Events/Event.h
+++ Hazel/src/Hazel/Events/Event.h
@@ -53,20 +53,18 @@ namespace Hazel {
 
 	class EventDispatcher
 	{
-		template<typename T>
-		using EventFn = std::function<bool(T&)>;
 	public:
 		EventDispatcher(Event& event)
 			: m_Event(event)
 		{
 		}
 
-		template<typename T>
-		bool Dispatch(EventFn<T> func)
+		template<typename T, typename F>
+		bool Dispatch(const F& func)
 		{
 			if (m_Event.GetEventType() == T::GetStaticType())
 			{
-				m_Event.Handled = func(*(T*)&m_Event);
+				m_Event.Handled = func(static_cast<T&>(m_Event));
 				return true;
 			}
 			return false;
