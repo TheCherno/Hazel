ac9cedf22acb39f98aea3ee25a89d91166dc300e
Ryan <stunthound@gmail.com>
2019-08-26 21:18:35 -0400

    Implement WindowMode

--- Hazel/src/Hazel/Window.h
+++ Hazel/src/Hazel/Window.h
@@ -6,17 +6,26 @@
 #include "Hazel/Events/Event.h"
 
 namespace Hazel {
+	
+	enum WindowMode
+	{
+		Windowed,
+		Borderless,
+		FullScreen
+	};
 
 	struct WindowProps
 	{
 		std::string Title;
 		unsigned int Width;
 		unsigned int Height;
+		WindowMode Mode;
 
 		WindowProps(const std::string& title = "Hazel Engine",
 			        unsigned int width = 1280,
-			        unsigned int height = 720)
-			: Title(title), Width(width), Height(height)
+			        unsigned int height = 720,
+			   	WindowMode mode = WindowMode::Windowed)
+			: Title(title), Width(width), Height(height), Mode(mode)
 		{
 		}
 	};
@@ -38,6 +47,7 @@ namespace Hazel {
 		virtual void SetEventCallback(const EventCallbackFn& callback) = 0;
 		virtual void SetVSync(bool enabled) = 0;
 		virtual bool IsVSync() const = 0;
+		virtual void SetWindowMode(const WindowMode& mode) = 0;
 
 		virtual void* GetNativeWindow() const = 0;
 
