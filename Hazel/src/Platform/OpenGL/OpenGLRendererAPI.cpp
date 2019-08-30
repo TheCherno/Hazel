f97b6e1359d5ebdf9bd5a411faadcc1f9442418b
ilikenoodles2 <36080054+ilikenoodles2@users.noreply.github.com>
2019-08-30 18:10:17 -0500

    Update OpenGLRendererAPI.cpp

--- Hazel/src/Platform/OpenGL/OpenGLRendererAPI.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLRendererAPI.cpp
@@ -31,6 +31,8 @@ namespace Hazel {
 		glEnable(GL_DEBUG_OUTPUT);
 		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
 		glDebugMessageCallback(OpenGLMessageCallback, nullptr);
+		
+		glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_NOTIFICATION, 0, NULL, GL_FALSE);
 	#endif
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
