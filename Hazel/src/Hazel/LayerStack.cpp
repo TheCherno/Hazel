c163d349362b449d7f3e1cea7efce0a40abfad82
Isho Antar <isho.antar1@gmail.com>
2019-09-15 18:02:33 +0300

    Call the layers OnDetach method before deleting it.

--- Hazel/src/Hazel/LayerStack.cpp
+++ Hazel/src/Hazel/LayerStack.cpp
@@ -10,7 +10,10 @@ namespace Hazel {
 	LayerStack::~LayerStack()
 	{
 		for (Layer* layer : m_Layers)
+		{
+			layer->OnDetach();
 			delete layer;
+		}
 	}
 
 	void LayerStack::PushLayer(Layer* layer)
