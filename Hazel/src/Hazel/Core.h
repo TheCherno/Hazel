23aa48611677b2ba52dbedc4b4b4f8726f4f5058
Isho Antar <isho.antar1@gmail.com>
2019-09-16 03:59:55 +0300

    Automated platform detection using predefined macro's; and removed the macro from premake5.lua since it's not needed anymore

--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -2,6 +2,42 @@
 
 #include <memory>
 
+#ifdef _WIN32
+	/* Windows x64/x86 */
+	#ifdef _WIN64
+		/* Windows x64  */
+		#define HZ_PLATFORM_WINDOWS
+	#else
+		/* Windows x86 */
+		#error "x86 Builds aren't supported"
+	#endif
+#elif defined(__APPLE__) || defined(__MACH__)
+	#include <TargetConditionals.h>
+	/* TARGET_OS_MAC exists on all the platforms
+	 * so we must check all of them (in this order)
+	 * to ensure that we're running on MAC
+	 * and not some other Apple platform */
+	#if TARGET_IPHONE_SIMULATOR == 1
+		#error "IOS simulator is not supported"
+	#elif TARGET_OS_IPHONE == 1
+		#error "IOS is not supported"
+	#elif TARGET_OS_MAC == 1
+		#define HZ_PLATFORM_MACOS
+	#else
+		#error "Unknown Apple platform"
+	#endif
+/* We also have to check __ANDROID__ before __linux__
+ * since android is based on the linux kernel
+ * it has __linux__ defined */
+#elif defined(__ANDROID__)
+	#error "Android is not supported!"
+#elif defined(__linux__)
+	#define HZ_PLATFORM_LINUX
+#else
+	/* Unknown compiler/platform */
+	#error "Unknown platform!"
+#endif
+
 #ifdef HZ_PLATFORM_WINDOWS
 #if HZ_DYNAMIC_LINK
 	#ifdef HZ_BUILD_DLL
