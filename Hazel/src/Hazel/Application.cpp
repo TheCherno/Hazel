699aa98240aef2d40ecfeb5c55e2edbf9ff30044
Ryan Bernstein <ryanbbernstein@gmail.com>
2019-09-19 21:50:39 -0700

    removing std::bind

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -11,8 +11,6 @@
 
 namespace Hazel {
 
-#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
-
 	Application* Application::s_Instance = nullptr;
 
 	Application::Application()
--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -30,8 +30,6 @@
 
 #define BIT(x) (1 << x)
 
-#define HZ_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
-
 namespace Hazel {
 
 	template<typename T>
