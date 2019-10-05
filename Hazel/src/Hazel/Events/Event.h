fb841f8c0100c7c090e03dc62cab64c0bbd2ddff
Brian Orora <brianorora@gmail.com>
2019-10-05 18:01:16 +0300

    Remove unnecessary token-pasting operator in EVENT_CLASS_TYPE (#113)
    avoids warnings from compiler (or even errors in some compilers)

--- Hazel/src/Hazel/Events/Event.h
+++ Hazel/src/Hazel/Events/Event.h
@@ -29,7 +29,7 @@ namespace Hazel {
 		EventCategoryMouseButton    = BIT(4)
 	};
 
-#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::##type; }\
+#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::type; }\
 								virtual EventType GetEventType() const override { return GetStaticType(); }\
 								virtual const char* GetName() const override { return #type; }
 
