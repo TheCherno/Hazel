ba289d8d0b03c204114b29c6b6a812aa404e1235
lovely_santa <laurens.scheldeman@gmail.com>
2019-10-05 19:13:58 +0200

    Small changes to indentations & names

--- Hazel/src/Hazel/Core/Core.h
+++ Hazel/src/Hazel/Core/Core.h
@@ -2,6 +2,7 @@
 
 #include <memory>
 
+// Platform detection using predefined macros
 #ifdef _WIN32
 	/* Windows x64/x86 */
 	#ifdef _WIN64
@@ -9,7 +10,7 @@
 		#define HZ_PLATFORM_WINDOWS
 	#else
 		/* Windows x86 */
-		#error "x86 Builds aren't supported"
+		#error "x86 Builds are not supported!"
 	#endif
 #elif defined(__APPLE__) || defined(__MACH__)
 	#include <TargetConditionals.h>
@@ -18,39 +19,45 @@
 	 * to ensure that we're running on MAC
 	 * and not some other Apple platform */
 	#if TARGET_IPHONE_SIMULATOR == 1
-		#error "IOS simulator is not supported"
+		#error "IOS simulator is not supported!"
 	#elif TARGET_OS_IPHONE == 1
-		#error "IOS is not supported"
+		#define HZ_PLATFORM_IOS
+		#error "IOS is not supported!"
 	#elif TARGET_OS_MAC == 1
 		#define HZ_PLATFORM_MACOS
+		#error "MacOS is not supported!"
 	#else
-		#error "Unknown Apple platform"
+		#error "Unknown Apple platform!"
 	#endif
 /* We also have to check __ANDROID__ before __linux__
  * since android is based on the linux kernel
  * it has __linux__ defined */
 #elif defined(__ANDROID__)
+	#define HZ_PLATFORM_ANDROID
 	#error "Android is not supported!"
 #elif defined(__linux__)
 	#define HZ_PLATFORM_LINUX
+	#error "Linux is not supported!"
 #else
 	/* Unknown compiler/platform */
 	#error "Unknown platform!"
-#endif
+#endif // End of platform detection
 
+
+// DLL support
 #ifdef HZ_PLATFORM_WINDOWS
-#if HZ_DYNAMIC_LINK
-	#ifdef HZ_BUILD_DLL
-		#define HAZEL_API __declspec(dllexport)
+	#if HZ_DYNAMIC_LINK
+		#ifdef HZ_BUILD_DLL
+			#define HAZEL_API __declspec(dllexport)
+		#else
+			#define HAZEL_API __declspec(dllimport)
+		#endif
 	#else
-		#define HAZEL_API __declspec(dllimport)
+		#define HAZEL_API
 	#endif
 #else
-    #define HAZEL_API
-#endif
-#else
 	#error Hazel only supports Windows!
-#endif
+#endif // End of DLL support
 
 #ifdef HZ_DEBUG
 	#define HZ_ENABLE_ASSERTS
