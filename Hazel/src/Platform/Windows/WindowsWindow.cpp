6c33e0ef66275e942188498ee3e588f2be8def1d
lovely_santa <laurens.scheldeman@gmail.com>
2019-10-06 20:04:31 +0200

    Added GraphicsContext::Create function

--- Hazel/src/Platform/Windows/WindowsWindow.cpp
+++ Hazel/src/Platform/Windows/WindowsWindow.cpp
@@ -50,8 +50,7 @@ namespace Hazel {
 
 		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
 
-		m_Context = CreateScope<OpenGLContext>(m_Window);
-
+		m_Context = GraphicsContext::Create(m_Window);
 		m_Context->Init();
 
 		glfwSetWindowUserPointer(m_Window, &m_Data);
