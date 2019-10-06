6fe76b526acb9b716aa02c7dc94b5579b6954c36
lovely_santa <laurens.scheldeman@gmail.com>
2019-10-06 20:12:01 +0200

    Added RendererAPI::Create function

--- Hazel/src/Hazel/Renderer/RendererAPI.h
+++ Hazel/src/Hazel/Renderer/RendererAPI.h
@@ -22,6 +22,8 @@ namespace Hazel {
 		virtual void DrawIndexed(const Ref<VertexArray>& vertexArray) = 0;
 
 		inline static API GetAPI() { return s_API; }
+		static Scope<RendererAPI> Create();
+
 	private:
 		static API s_API;
 	};
