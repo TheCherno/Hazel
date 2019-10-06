998fad1379cee4850828ff507a9e993fa4c16d00
lovely_santa <laurens.scheldeman@gmail.com>
2019-10-06 20:10:58 +0200

    Updated Texture2D::Create function

--- Hazel/src/Hazel/Renderer/Texture.cpp
+++ Hazel/src/Hazel/Renderer/Texture.cpp
@@ -11,7 +11,7 @@ namespace Hazel {
 		switch (Renderer::GetAPI())
 		{
 			case RendererAPI::API::None:    HZ_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
-			case RendererAPI::API::OpenGL:  return std::make_shared<OpenGLTexture2D>(path);
+			case RendererAPI::API::OpenGL:  return CreateRef<OpenGLTexture2D>(path);
 		}
 
 		HZ_CORE_ASSERT(false, "Unknown RendererAPI!");
