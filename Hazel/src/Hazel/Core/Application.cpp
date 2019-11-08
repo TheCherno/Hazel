235d304ebbd1efd32c255c1a05fed73838bde6cc
Jack Punter <punter.jack@yahoo.com>
2019-11-08 00:07:15 +0000

    Iterators in LayerStack
    Added more Iterator wrappers to the Layer Stack. Utilized this in Application to remove "Hacky" reverse iteration through the layer stack.

--- Hazel/src/Hazel/Core/Application.cpp
+++ Hazel/src/Hazel/Core/Application.cpp
@@ -45,9 +45,9 @@ namespace Hazel {
 		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
 		dispatcher.Dispatch<WindowResizeEvent>(BIND_EVENT_FN(OnWindowResize));
 
-		for (auto it = m_LayerStack.end(); it != m_LayerStack.begin(); )
+		for (auto it = m_LayerStack.rbegin(); it != m_LayerStack.rend(); ++it)
 		{
-			(*--it)->OnEvent(e);
+			(*it)->OnEvent(e);
 			if (e.Handled)
 				break;
 		}
