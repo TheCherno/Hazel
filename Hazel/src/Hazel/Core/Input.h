0fca458cf8767b5f02586a1c6f4b22038fcea89e
Adri Wessels <a3wes@icloud.com>
2019-11-13 17:19:22 +0200

    Removed old TODO and fixed formatting

--- Hazel/src/Hazel/Core/Input.h
+++ Hazel/src/Hazel/Core/Input.h
@@ -15,11 +15,7 @@ namespace Hazel {
 		inline static bool IsKeyPressed(int keycode) { return s_Instance->IsKeyPressedImpl(keycode); }
 
 		inline static uint16_t GetKeyRepeatCount(int keycode) { return s_KeyRepeatCounts.count(keycode) == 0 ? 0 : s_KeyRepeatCounts[keycode]; }
-		inline static uint16_t IncrementKeyRepeatCount(int keycode)
-		{
-			//TODO: Test if this works when keycode not in map
-			return s_KeyRepeatCounts[keycode]++;
-		}
+		inline static uint16_t IncrementKeyRepeatCount(int keycode) { return s_KeyRepeatCounts[keycode]++; }
 		inline static void ResetKeyRepeatCount(int keycode) { s_KeyRepeatCounts.erase(keycode); }
 
 		inline static bool IsMouseButtonPressed(int button) { return s_Instance->IsMouseButtonPressedImpl(button); }
