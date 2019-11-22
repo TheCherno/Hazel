3aff8a00f063efb429d5e0e766820b9df9243207
GabeRundlett <gabe@rundlett.com>
2019-11-21 20:48:48 -0500

    Updated Instrumentor to choose the best 'funcsig' macro

--- Hazel/src/Hazel/Debug/Instrumentor.h
+++ Hazel/src/Hazel/Debug/Instrumentor.h
@@ -1,120 +1,140 @@
 #pragma once
 
-#include <algorithm>
-#include <chrono>
-#include <fstream>
 #include <string>
+#include <chrono>
+#include <algorithm>
+#include <fstream>
 
 #include <thread>
 
 namespace Hazel {
-    struct ProfileResult {
-        std::string Name;
-        long long Start, End;
-        uint32_t ThreadID;
-    };
+	struct ProfileResult
+	{
+		std::string Name;
+		long long Start, End;
+		uint32_t ThreadID;
+	};
 
-    struct InstrumentationSession {
-        std::string Name;
-    };
+	struct InstrumentationSession
+	{
+		std::string Name;
+	};
 
-    class Instrumentor {
-      private:
-        InstrumentationSession *m_CurrentSession;
-        std::ofstream m_OutputStream;
-        int m_ProfileCount;
+	class Instrumentor
+	{
+	private:
+		InstrumentationSession* m_CurrentSession;
+		std::ofstream m_OutputStream;
+		int m_ProfileCount;
+	public:
+		Instrumentor()
+			: m_CurrentSession(nullptr), m_ProfileCount(0)
+		{
+		}
 
-      public:
-        Instrumentor() : m_CurrentSession(nullptr), m_ProfileCount(0) {}
+		void BeginSession(const std::string& name, const std::string& filepath = "results.json")
+		{
+			m_OutputStream.open(filepath);
+			WriteHeader();
+			m_CurrentSession = new InstrumentationSession{ name };
+		}
 
-        void BeginSession(const std::string &name, const std::string &filepath = "results.json") {
-            m_OutputStream.open(filepath);
-            WriteHeader();
-            m_CurrentSession = new InstrumentationSession{name};
-        }
+		void EndSession()
+		{
+			WriteFooter();
+			m_OutputStream.close();
+			delete m_CurrentSession;
+			m_CurrentSession = nullptr;
+			m_ProfileCount = 0;
+		}
 
-        void EndSession() {
-            WriteFooter();
-            m_OutputStream.close();
-            delete m_CurrentSession;
-            m_CurrentSession = nullptr;
-            m_ProfileCount = 0;
-        }
+		void WriteProfile(const ProfileResult& result)
+		{
+			if (m_ProfileCount++ > 0)
+				m_OutputStream << ",";
 
-        void WriteProfile(const ProfileResult &result) {
-            if (m_ProfileCount++ > 0) m_OutputStream << ",";
+			std::string name = result.Name;
+			std::replace(name.begin(), name.end(), '"', '\'');
 
-            std::string name = result.Name;
-            std::replace(name.begin(), name.end(), '"', '\'');
+			m_OutputStream << "{";
+			m_OutputStream << "\"cat\":\"function\",";
+			m_OutputStream << "\"dur\":" << (result.End - result.Start) << ',';
+			m_OutputStream << "\"name\":\"" << name << "\",";
+			m_OutputStream << "\"ph\":\"X\",";
+			m_OutputStream << "\"pid\":0,";
+			m_OutputStream << "\"tid\":" << result.ThreadID << ",";
+			m_OutputStream << "\"ts\":" << result.Start;
+			m_OutputStream << "}";
 
-            m_OutputStream << "{";
-            m_OutputStream << "\"cat\":\"function\",";
-            m_OutputStream << "\"dur\":" << (result.End - result.Start) << ',';
-            m_OutputStream << "\"name\":\"" << name << "\",";
-            m_OutputStream << "\"ph\":\"X\",";
-            m_OutputStream << "\"pid\":0,";
-            m_OutputStream << "\"tid\":" << result.ThreadID << ",";
-            m_OutputStream << "\"ts\":" << result.Start;
-            m_OutputStream << "}";
+			m_OutputStream.flush();
+		}
 
-            m_OutputStream.flush();
-        }
+		void WriteHeader()
+		{
+			m_OutputStream << "{\"otherData\": {},\"traceEvents\":[";
+			m_OutputStream.flush();
+		}
 
-        void WriteHeader() {
-            m_OutputStream << "{\"otherData\": {},\"traceEvents\":[";
-            m_OutputStream.flush();
-        }
+		void WriteFooter()
+		{
+			m_OutputStream << "]}";
+			m_OutputStream.flush();
+		}
 
-        void WriteFooter() {
-            m_OutputStream << "]}";
-            m_OutputStream.flush();
-        }
+		static Instrumentor& Get()
+		{
+			static Instrumentor instance;
+			return instance;
+		}
+	};
 
-        static Instrumentor &Get() {
-            static Instrumentor instance;
-            return instance;
-        }
-    };
+	class InstrumentationTimer
+	{
+	public:
+		InstrumentationTimer(const char* name)
+			: m_Name(name), m_Stopped(false)
+		{
+			m_StartTimepoint = std::chrono::high_resolution_clock::now();
+		}
 
-    class InstrumentationTimer {
-      public:
-        InstrumentationTimer(const char *name) : m_Name(name), m_Stopped(false) {
-            m_StartTimepoint = std::chrono::high_resolution_clock::now();
-        }
+		~InstrumentationTimer()
+		{
+			if (!m_Stopped)
+				Stop();
+		}
 
-        ~InstrumentationTimer() {
-            if (!m_Stopped) Stop();
-        }
+		void Stop()
+		{
+			auto endTimepoint = std::chrono::high_resolution_clock::now();
 
-        void Stop() {
-            auto endTimepoint = std::chrono::high_resolution_clock::now();
+			long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
+			long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
 
-            long long start =
-                std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
-            long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
+			uint32_t threadID = std::hash<std::thread::id>{}(std::this_thread::get_id());
+			Instrumentor::Get().WriteProfile({ m_Name, start, end, threadID });
 
-            uint32_t threadID = std::hash<std::thread::id>{}(std::this_thread::get_id());
-            Instrumentor::Get().WriteProfile({m_Name, start, end, threadID});
-
-            m_Stopped = true;
-        }
-
-      private:
-        const char *m_Name;
-        std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTimepoint;
-        bool m_Stopped;
-    };
-} // namespace Hazel
+			m_Stopped = true;
+		}
+	private:
+		const char* m_Name;
+		std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTimepoint;
+		bool m_Stopped;
+	};
+}
 
 #define HZ_PROFILE 1
 #if HZ_PROFILE
-	#ifndef __FUNCSIG__
-		#define __FUNCSIG__ __PRETTY_FUNCTION__
+	#if defined(__FUNCSIG__)
+		#define HZ_FUNC_SIG __FUNCSIG__
+	#elif defined(__PRETTY_FUNCTION__)
+		#define HZ_FUNC_SIG __PRETTY_FUNCTION__
+	#else
+		#define HZ_FUNC_SIG __FUNCTION__
 	#endif
 	#define HZ_PROFILE_BEGIN_SESSION(name, filepath) ::Hazel::Instrumentor::Get().BeginSession(name, filepath)
 	#define HZ_PROFILE_END_SESSION() ::Hazel::Instrumentor::Get().EndSession()
 	#define HZ_PROFILE_SCOPE(name) ::Hazel::InstrumentationTimer timer##__LINE__(name);
-	#define HZ_PROFILE_FUNCTION() HZ_PROFILE_SCOPE(__FUNCSIG__)
+	#define HZ_PROFILE_FUNCTION() HZ_PROFILE_SCOPE(HZ_FUNC_SIG)
 #else
 	#define HZ_PROFILE_BEGIN_SESSION(name, filepath)
 	#define HZ_PROFILE_END_SESSION()
