e8867656db9592a02c68e02370d2d6de77caaacc
freeman40 <jrfwaite@gmail.com>
2019-12-01 18:15:17 +1300

    Remove profile result json files from version control. Change EntryPoint.h so that profile result files go to temp directory.

--- Hazel/src/Hazel/Core/EntryPoint.h
+++ Hazel/src/Hazel/Core/EntryPoint.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "Hazel/Core/Core.h"
 
+#ifdef HZ_PROFILE
+#include <filesystem>
+#endif
+
 #ifdef HZ_PLATFORM_WINDOWS
 
 extern Hazel::Application* Hazel::CreateApplication();
@@ -9,15 +13,15 @@ int main(int argc, char** argv)
 {
 	Hazel::Log::Init();
 
-	HZ_PROFILE_BEGIN_SESSION("Startup", "HazelProfile-Startup.json");
+	HZ_PROFILE_BEGIN_SESSION("Startup", (std::filesystem::temp_directory_path() / "HazelProfile-Startup.json").string());
 	auto app = Hazel::CreateApplication();
 	HZ_PROFILE_END_SESSION();
 
-	HZ_PROFILE_BEGIN_SESSION("Runtime", "HazelProfile-Runtime.json");
+	HZ_PROFILE_BEGIN_SESSION("Runtime", (std::filesystem::temp_directory_path() / "HazelProfile-Runtime.json").string());
 	app->Run();
 	HZ_PROFILE_END_SESSION();
 
-	HZ_PROFILE_BEGIN_SESSION("Startup", "HazelProfile-Shutdown.json");
+	HZ_PROFILE_BEGIN_SESSION("Startup", (std::filesystem::temp_directory_path() / "HazelProfile-Shutdown.json").string());
 	delete app;
 	HZ_PROFILE_END_SESSION();
 }
