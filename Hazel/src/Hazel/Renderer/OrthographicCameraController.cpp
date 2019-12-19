e5c59b32daae73737ffa34395715bd2a242e8b51
3x3y3z3t <>
2019-12-19 16:25:03 +0700

    Update OrthographicCameraController.
    Update OrthographicCameraController:
      - CameraTranslationSpeed scales relatively to ZoomLevel.
      - ZoomLevel can be set manually and override min/max allowed zoom level.
      - Minor improvements.

--- Hazel/src/Hazel/Renderer/OrthographicCameraController.cpp
+++ Hazel/src/Hazel/Renderer/OrthographicCameraController.cpp
@@ -7,7 +7,7 @@
 namespace Hazel {
 
 	OrthographicCameraController::OrthographicCameraController(float aspectRatio, bool rotation)
-		: m_AspectRatio(aspectRatio), m_Camera(-m_AspectRatio * m_ZoomLevel, m_AspectRatio * m_ZoomLevel, -m_ZoomLevel, m_ZoomLevel), m_Rotation(rotation)
+		: m_AspectRatio(aspectRatio), m_Camera(-m_AspectRatio * m_ZoomLevel, m_AspectRatio * m_ZoomLevel, -m_ZoomLevel, m_ZoomLevel), m_EnableRotation(rotation)
 	{
 	}
 
@@ -37,7 +37,7 @@ namespace Hazel {
 			m_CameraPosition.y -= cos(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
 		}
 
-		if (m_Rotation)
+		if (m_EnableRotation)
 		{
 			if (Input::IsKeyPressed(HZ_KEY_Q))
 				m_CameraRotation += m_CameraRotationSpeed * ts;
@@ -54,7 +54,18 @@ namespace Hazel {
 
 		m_Camera.SetPosition(m_CameraPosition);
 
-		m_CameraTranslationSpeed = m_ZoomLevel;
+		//m_CameraTranslationSpeed = m_ZoomLevel;
+	}
+
+	void OrthographicCameraController::SetZoomLevel(float level)
+	{
+		HZ_CORE_ASSERT(level > 0.0f, "Zoom Level have to be bigger than 0.0f!");
+
+		// translation speed should scale relative to the zoom level changes;
+		float diff = level / m_ZoomLevel; // zoom in -> fast translation, zoom out -> slow translation;
+		m_CameraTranslationSpeed *= diff;
+		m_ZoomLevel = level;
+		UpdateProjectionMatrix();
 	}
 
 	void OrthographicCameraController::OnEvent(Event& e)
@@ -70,9 +81,19 @@ namespace Hazel {
 	{
 		HZ_PROFILE_FUNCTION();
 
-		m_ZoomLevel -= e.GetYOffset() * 0.25f;
-		m_ZoomLevel = std::max(m_ZoomLevel, 0.25f);
-		m_Camera.SetProjection(-m_AspectRatio * m_ZoomLevel, m_AspectRatio * m_ZoomLevel, -m_ZoomLevel, m_ZoomLevel);
+		float zoomLevel = m_ZoomLevel - e.GetYOffset() * m_ZoomSpeed;
+		/* Hazel's current "Zoom Level" definition makes std::clamp confuses:
+		 *   - 0.25f Zoom Level -> 4x magnification
+		 *   - 2.0f Zoom Level -> 0.5x magnification
+		 * so the min and max value of "Zoom Level" and "Magnification Level" is the invertion of each other.
+		 * If we set MaxZoomLevel to 0.25f (4x) and MinZoomLevel to 2.0f (0.5x) then the user will confuses 
+		 * "why Max is smaller than Min??".
+		 *
+		 * Suggestion: Changing the definition to "Magnification Level".
+		 */
+		zoomLevel = std::clamp(zoomLevel, m_MinZoomLevel, m_MaxZoomLevel);
+		SetZoomLevel(zoomLevel);
+		UpdateProjectionMatrix();
 		return false;
 	}
 
@@ -81,7 +102,7 @@ namespace Hazel {
 		HZ_PROFILE_FUNCTION();
 
 		m_AspectRatio = (float)e.GetWidth() / (float)e.GetHeight();
-		m_Camera.SetProjection(-m_AspectRatio * m_ZoomLevel, m_AspectRatio * m_ZoomLevel, -m_ZoomLevel, m_ZoomLevel);
+		UpdateProjectionMatrix();
 		return false;
 	}
 
--- Hazel/src/Hazel/Renderer/OrthographicCameraController.h
+++ Hazel/src/Hazel/Renderer/OrthographicCameraController.h
@@ -20,20 +20,33 @@ namespace Hazel {
 		const OrthographicCamera& GetCamera() const { return m_Camera; }
 
 		float GetZoomLevel() const { return m_ZoomLevel; }
-		void SetZoomLevel(float level) { m_ZoomLevel = level; }
+		/* Setting zoom level using this function will override min/max zoom level. */
+		void SetZoomLevel(float level);
+
+		float GetMagnificationZoomLevel() const { return 1.0f / m_ZoomLevel; }
+		void SetMagnificationZoomLevel(float magnificationLevel) { if (magnificationLevel != 0.0f) SetZoomLevel(1.0f / magnificationLevel); }
+
+		bool IsRotationEnabled() const { return m_EnableRotation; }
+		void SetEnableRotation(bool enabled) { m_EnableRotation = enabled; }
 	private:
 		bool OnMouseScrolled(MouseScrolledEvent& e);
 		bool OnWindowResized(WindowResizeEvent& e);
+		void UpdateProjectionMatrix() { m_Camera.SetProjection(-m_AspectRatio * m_ZoomLevel, m_AspectRatio * m_ZoomLevel, -m_ZoomLevel, m_ZoomLevel); }
 	private:
 		float m_AspectRatio;
 		float m_ZoomLevel = 1.0f;
-		OrthographicCamera m_Camera;
-
-		bool m_Rotation;
-
 		glm::vec3 m_CameraPosition = { 0.0f, 0.0f, 0.0f };
 		float m_CameraRotation = 0.0f; //In degrees, in the anti-clockwise direction
-		float m_CameraTranslationSpeed = 5.0f, m_CameraRotationSpeed = 180.0f;
+		bool m_EnableRotation;
+
+		OrthographicCamera m_Camera;
+
+		// TODO: add helper function to get/set those values, or public them;
+		float m_CameraTranslationSpeed = 2.0f;
+		float m_CameraRotationSpeed = 180.0f; //Also in degrees, in the anti-clockwise direction
+		float m_ZoomSpeed = 0.25f;
+		float m_MaxZoomLevel = 2.0f;
+		float m_MinZoomLevel = 0.25f;
 	};
 
 }
