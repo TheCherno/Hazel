554fb3fcad0f7e0cd9f5f690434708e6b098618d
LewyIsChewy <42196997+LewyIsChewy@users.noreply.github.com>
2020-01-12 22:37:52 +1100

    compile time check of window type and input type (#190)
    Adds Window::Create and Input::Create function into one place

--- Hazel/src/Platform/Windows/WindowsWindow.cpp
+++ Hazel/src/Platform/Windows/WindowsWindow.cpp
@@ -20,11 +20,6 @@ namespace Hazel {
 		HZ_CORE_ERROR("GLFW Error ({0}): {1}", error, description);
 	}
 
-	Scope<Window> Window::Create(const WindowProps& props)
-	{
-		return CreateScope<WindowsWindow>(props);
-	}
-
 	WindowsWindow::WindowsWindow(const WindowProps& props)
 	{
 		HZ_PROFILE_FUNCTION();
