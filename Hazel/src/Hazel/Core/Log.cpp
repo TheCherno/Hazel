09c2cd92b25aa0f57b51e69df141eb5602e50df6
HerrDingenz <ivoka76@gmail.com>
2020-01-31 23:24:10 +0100

    changed to use sinks

--- Hazel/src/Hazel/Core/Log.cpp
+++ Hazel/src/Hazel/Core/Log.cpp
@@ -6,37 +6,32 @@
 
 namespace Hazel {
 
-	#ifdef HZ_DEBUG
-		Ref<spdlog::logger> Log::s_CoreDebugLogger;
-		Ref<spdlog::logger> Log::s_ClientDebugLogger;
-	#endif // HZ_DEBUG
+	Ref<spdlog::logger> Log::s_CoreLogger;
+	Ref<spdlog::logger> Log::s_ClientLogger;
 
-	Ref<spdlog::logger> Log::s_CoreFileLogger;
-	Ref<spdlog::logger> Log::s_ClientFileLogger;
 
 	void Log::Init()
 	{
-		#ifdef HZ_DEBUG
-			s_CoreDebugLogger = spdlog::stdout_color_mt("HAZEL");
-			HZ_ASSERT(s_CoreDebugLogger, "Could not initialize Core Debug Logger!");
-			s_CoreDebugLogger->set_pattern("%^[%T]: %v%$");
-			s_CoreDebugLogger->set_level(spdlog::level::trace);
+		// create the sinks
+		std::vector<spdlog::sink_ptr> coreSinks;
+		coreSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>()); // VS debug console
+		coreSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Hazel-Core.log", true)); // CORE file logger
 
-			s_ClientDebugLogger = spdlog::stdout_color_mt("APP");
-			HZ_ASSERT(s_ClientDebugLogger, "Could not initialize App Debug Logger!");
-			s_ClientDebugLogger->set_pattern("%^[%T]: %v%$");
-			s_ClientDebugLogger->set_level(spdlog::level::trace);
-		#endif // HZ_DEBUG
+		std::vector<spdlog::sink_ptr> clientSinks;
+		clientSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>()); // VS debug console
+		clientSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Hazel-Client.log", true)); // CLIENT file logger
 
-		s_CoreFileLogger = spdlog::basic_logger_mt("HAZEL-FILE", "hazel-log.txt", true);
-		HZ_ASSERT(s_CoreFileLogger, "Could not initialize Core File Logger!");
-		s_CoreFileLogger->set_pattern("%v");
-		s_CoreFileLogger->set_level(spdlog::level::trace);
+		// create the loggers
+		s_CoreLogger = std::make_shared<spdlog::logger>("HAZEL", begin(coreSinks), end(coreSinks));
+		spdlog::register_logger(s_CoreLogger);
+		s_ClientLogger = std::make_shared<spdlog::logger>("APP", begin(clientSinks), end(clientSinks));
+		spdlog::register_logger(s_ClientLogger);
 
-		s_ClientFileLogger = spdlog::basic_logger_mt("CLIENT-FILE", "client-log.txt", true);
-		HZ_ASSERT(s_ClientFileLogger, "Could not initialize Client File Logger!");
-		s_ClientFileLogger->set_pattern("%v");
-		s_ClientFileLogger->set_level(spdlog::level::trace);
+		// configure the loggers
+		spdlog::set_pattern("%^[%T] %n: %v%$");
+		s_CoreLogger->set_level(spdlog::level::trace);
+		s_ClientLogger->set_level(spdlog::level::trace);
+
 	}
 
 }
--- Hazel/src/Hazel/Core/Log.h
+++ Hazel/src/Hazel/Core/Log.h
@@ -12,129 +12,25 @@ namespace Hazel {
 	public:
 		static void Init();
 
-		#ifdef HZ_DEBUG
-			inline static Ref<spdlog::logger>& GetCoreDebugLogger() { return s_CoreDebugLogger; }
-			inline static Ref<spdlog::logger>& GetClientDebugLogger() { return s_ClientDebugLogger; }
-		#endif // HZ_DEBUG
-
-		inline static Ref<spdlog::logger>& GetCoreFileLogger() { return s_CoreFileLogger; }
-		inline static Ref<spdlog::logger>& GetClientFileLogger()	{ return s_ClientFileLogger; }
-
+		inline static Ref<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static Ref<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
 	private:
-		#ifdef HZ_DEBUG
-			static Ref<spdlog::logger> s_CoreDebugLogger;
-			static Ref<spdlog::logger> s_ClientDebugLogger;
-		#endif // HZ_DEBUG
-
-		static Ref<spdlog::logger> s_CoreFileLogger;
-		static Ref<spdlog::logger> s_ClientFileLogger;
+		static Ref<spdlog::logger> s_CoreLogger;
+		static Ref<spdlog::logger> s_ClientLogger;
 	};
 
 }
 
-#pragma region Core Log Macros
-#ifdef HZ_DEBUG
-#define HZ_CORE_TRACE(...)										\
-			Hazel::Log::GetCoreFileLogger()->trace(__VA_ARGS__);\
-			Hazel::Log::GetCoreFileLogger()->flush();			\
-			Hazel::Log::GetCoreDebugLogger()->trace(__VA_ARGS__)
-#define HZ_CORE_INFO(...)										\
-			Hazel::Log::GetCoreFileLogger()->info(__VA_ARGS__);	\
-			Hazel::Log::GetCoreFileLogger()->flush();			\
-			Hazel::Log::GetCoreDebugLogger()->info(__VA_ARGS__)
-#define HZ_CORE_WARN(...)												\
-			Hazel::Log::GetCoreFileLogger()->warn("\n*** Warning ***");	\
-			Hazel::Log::GetCoreFileLogger()->warn(__VA_ARGS__);			\
-			Hazel::Log::GetCoreFileLogger()->warn("");					\
-			Hazel::Log::GetCoreFileLogger()->flush();					\
-			Hazel::Log::GetCoreDebugLogger()->warn(__VA_ARGS__)
-#define HZ_CORE_ERROR(...)												\
-			Hazel::Log::GetCoreFileLogger()->error("\n*** Error ***");	\
-			Hazel::Log::GetCoreFileLogger()->error(__VA_ARGS__);		\
-			Hazel::Log::GetCoreFileLogger()->error("");					\
-			Hazel::Log::GetCoreFileLogger()->flush();					\
-			Hazel::Log::GetCoreDebugLogger()->error(__VA_ARGS__)
-#define HZ_CORE_CRITICAL(...)												\
-			Hazel::Log::GetCoreFileLogger()->critical("\n*** Critical ***");\
-			Hazel::Log::GetCoreFileLogger()->critical(__VA_ARGS__);			\
-			Hazel::Log::GetCoreFileLogger()->critical("");					\
-			Hazel::Log::GetCoreFileLogger()->flush();						\
-			Hazel::Log::GetCoreDebugLogger()->critical(__VA_ARGS__)
-#elif HZ_RELEASE
-#define HZ_CORE_TRACE(...)										\
-			Hazel::Log::GetCoreFileLogger()->trace(__VA_ARGS__);\
-			Hazel::Log::GetCoreFileLogger()->flush()
-#define HZ_CORE_INFO(...)										\
-			Hazel::Log::GetCoreFileLogger()->info(__VA_ARGS__);	\
-			Hazel::Log::GetCoreFileLogger()->flush()
-#define HZ_CORE_WARN(...)												\
-			Hazel::Log::GetCoreFileLogger()->warn("\n*** Warning ***");	\
-			Hazel::Log::GetCoreFileLogger()->warn(__VA_ARGS__);			\
-			Hazel::Log::GetCoreFileLogger()->warn("");					\
-			Hazel::Log::GetCoreFileLogger()->flush()						
-#define HZ_CORE_ERROR(...)												\
-			Hazel::Log::GetCoreFileLogger()->error("\n*** Error ***");	\
-			Hazel::Log::GetCoreFileLogger()->error(__VA_ARGS__);		\
-			Hazel::Log::GetCoreFileLogger()->error("");					\
-			Hazel::Log::GetCoreFileLogger()->flush()
-#define HZ_CORE_CRITICAL(...)												\
-			Hazel::Log::GetCoreFileLogger()->critical("\n*** Critical ***");\
-			Hazel::Log::GetCoreFileLogger()->critical(__VA_ARGS__);			\
-			Hazel::Log::GetCoreFileLogger()->critical("");					\
-			Hazel::Log::GetCoreFileLogger()->flush()
-#endif
-#pragma endregion
+// Core log macros
+#define HZ_CORE_TRACE(...)    ::Hazel::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define HZ_CORE_INFO(...)     ::Hazel::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define HZ_CORE_WARN(...)     ::Hazel::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define HZ_CORE_ERROR(...)    ::Hazel::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define HZ_CORE_CRITICAL(...) ::Hazel::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
-#pragma region Client Log Macros
-#ifdef HZ_DEBUG
-#define HZ_TRACE(...)												\
-			Hazel::Log::GetClientFileLogger()->trace(__VA_ARGS__);	\
-			Hazel::Log::GetClientFileLogger()->flush();				\
-			Hazel::Log::GetClientDebugLogger()->trace(__VA_ARGS__)
-#define HZ_INFO(...)												\
-			Hazel::Log::GetClientFileLogger()->info(__VA_ARGS__);	\
-			Hazel::Log::GetClientFileLogger()->flush();			\
-			Hazel::Log::GetClientDebugLogger()->info(__VA_ARGS__)
-#define HZ_WARN(...)														\
-			Hazel::Log::GetClientFileLogger()->warn("\n*** Warning ***");	\
-			Hazel::Log::GetClientFileLogger()->warn(__VA_ARGS__);			\
-			Hazel::Log::GetClientFileLogger()->warn("");					\
-			Hazel::Log::GetClientFileLogger()->flush();						\
-			Hazel::Log::GetClientDebugLogger()->warn(__VA_ARGS__)
-#define HZ_ERROR(...)													\
-			Hazel::Log::GetClientFileLogger()->error("\n*** Error ***");\
-			Hazel::Log::GetClientFileLogger()->error(__VA_ARGS__);		\
-			Hazel::Log::GetClientFileLogger()->error("");				\
-			Hazel::Log::GetClientFileLogger()->flush();					\
-			Hazel::Log::GetClientDebugLogger()->error(__VA_ARGS__)
-#define HZ_CRITICAL(...)														\
-			Hazel::Log::GetClientFileLogger()->critical("\n*** Critical ***");	\
-			Hazel::Log::GetClientFileLogger()->critical(__VA_ARGS__);			\
-			Hazel::Log::GetClientFileLogger()->critical("");					\
-			Hazel::Log::GetClientFileLogger()->flush();							\
-			Hazel::Log::GetClientDebugLogger()->critical(__VA_ARGS__)
-#elif HZ_RELEASE
-#define HZ_TRACE(...)												\
-			Hazel::Log::GetClientFileLogger()->trace(__VA_ARGS__);	\
-			Hazel::Log::GetClientFileLogger()->flush()
-#define HZ_INFO(...)												\
-			Hazel::Log::GetClientFileLogger()->info(__VA_ARGS__);	\
-			Hazel::Log::GetClientFileLogger()->flush()
-#define HZ_WARN(...)														\
-			Hazel::Log::GetClientFileLogger()->warn("\n*** Warning ***");	\
-			Hazel::Log::GetClientFileLogger()->warn(__VA_ARGS__);			\
-			Hazel::Log::GetClientFileLogger()->warn("");					\
-			Hazel::Log::GetClientFileLogger()->flush()						
-#define HZ_ERROR(...)													\
-			Hazel::Log::GetClientFileLogger()->error("\n*** Error ***");\
-			Hazel::Log::GetClientFileLogger()->error(__VA_ARGS__);		\
-			Hazel::Log::GetClientFileLogger()->error("");				\
-			Hazel::Log::GetClientFileLogger()->flush()
-#define HZ_CRITICAL(...)														\
-			Hazel::Log::GetClientFileLogger()->critical("\n*** Critical ***");	\
-			Hazel::Log::GetClientFileLogger()->critical(__VA_ARGS__);			\
-			Hazel::Log::GetClientFileLogger()->critical("");					\
-			Hazel::Log::GetClientFileLogger()->flush()
-#endif
-#pragma endregion
-
+// Client log macros
+#define HZ_TRACE(...)         ::Hazel::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define HZ_INFO(...)          ::Hazel::Log::GetClientLogger()->info(__VA_ARGS__)
+#define HZ_WARN(...)          ::Hazel::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define HZ_ERROR(...)         ::Hazel::Log::GetClientLogger()->error(__VA_ARGS__)
+#define HZ_CRITICAL(...)      ::Hazel::Log::GetClientLogger()->critical(__VA_ARGS__)
