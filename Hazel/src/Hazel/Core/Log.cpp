6483c4755d9cf0bd532b5bc6c423d56ded9d7ae3
HerrDingenz <ivoka76@gmail.com>
2020-02-01 23:53:27 +0100

    one sink and one log file only, different patterns

--- Hazel/src/Hazel/Core/Log.cpp
+++ Hazel/src/Hazel/Core/Log.cpp
@@ -1,35 +1,32 @@
 #include "hzpch.h"
 #include "Hazel/Core/Log.h"
 
+#include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/sinks/basic_file_sink.h>
 
 namespace Hazel {
 
 	Ref<spdlog::logger> Log::s_CoreLogger;
 	Ref<spdlog::logger> Log::s_ClientLogger;
-	Ref<spdlog::sinks::stdout_color_sink_mt> Log::s_ConsoleSink;
 
 	void Log::Init()
 	{
-		s_ConsoleSink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+		std::vector<spdlog::sink_ptr> logSinks;
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());// s_ConsoleSink);
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Hazel.log", true));
 
-		std::vector<spdlog::sink_ptr> coreSinks;
-		coreSinks.emplace_back(s_ConsoleSink);
-		coreSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Hazel-Core.log", true));
-
-		std::vector<spdlog::sink_ptr> clientSinks;
-		clientSinks.emplace_back(s_ConsoleSink);
-		clientSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Hazel-Client.log", true));
-
-		s_CoreLogger = std::make_shared<spdlog::logger>("HAZEL", begin(coreSinks), end(coreSinks));
+		s_CoreLogger = std::make_shared<spdlog::logger>("ENGINE", begin(logSinks), end(logSinks));
 		spdlog::register_logger(s_CoreLogger);
-		s_ClientLogger = std::make_shared<spdlog::logger>("APP", begin(clientSinks), end(clientSinks));
+		s_ClientLogger = std::make_shared<spdlog::logger>("CLIENT", begin(logSinks), end(logSinks));
 		spdlog::register_logger(s_ClientLogger);
 
-		spdlog::set_pattern("%^[%T] %n: %v%$");
+		s_CoreLogger->sinks()[0]->set_pattern("%^[%T] %n: %v%$");
+		s_CoreLogger->sinks()[1]->set_pattern("%n %L: %v");
 		s_CoreLogger->set_level(spdlog::level::trace);
-		s_ClientLogger->set_level(spdlog::level::trace);
 
+		s_ClientLogger->sinks()[0]->set_pattern("%^[%T] %n: %v%$");
+		s_ClientLogger->sinks()[1]->set_pattern("%n %L: %v");
+		s_ClientLogger->set_level(spdlog::level::trace);
 	}
 
 }
--- Hazel/src/Hazel/Core/Log.h
+++ Hazel/src/Hazel/Core/Log.h
@@ -4,7 +4,6 @@
 
 #include <spdlog/spdlog.h>
 #include <spdlog/fmt/ostr.h>
-#include <spdlog/sinks/stdout_color_sinks.h>
 
 namespace Hazel {
 
@@ -18,7 +17,6 @@ namespace Hazel {
 	private:
 		static Ref<spdlog::logger> s_CoreLogger;
 		static Ref<spdlog::logger> s_ClientLogger;
-		static Ref<spdlog::sinks::stdout_color_sink_mt> s_ConsoleSink;
 	};
 
 }
