eab8941e3b1d9dbeb5396b945168b910abfa5dc3
HerrDingenz <ivoka76@gmail.com>
2020-02-02 19:16:31 +0100

    requested changes

--- Hazel/src/Hazel/Core/Log.cpp
+++ Hazel/src/Hazel/Core/Log.cpp
@@ -25,7 +25,6 @@ namespace Hazel {
 		s_ClientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
 		spdlog::register_logger(s_ClientLogger);
 		s_ClientLogger->set_level(spdlog::level::trace);
-
 	}
 
 }
