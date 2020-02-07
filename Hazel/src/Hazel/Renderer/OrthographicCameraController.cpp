22ac3304523cdaf772ffdffc49d93b655a57bdf1
HerrDingenz <ivoka76@gmail.com>
2020-02-07 11:39:39 +0100

    key input fix

--- Hazel/src/Hazel/Renderer/OrthographicCameraController.cpp
+++ Hazel/src/Hazel/Renderer/OrthographicCameraController.cpp
@@ -4,6 +4,8 @@
 #include "Hazel/Core/Input.h"
 #include "Hazel/Core/KeyCodes.h"
 
+#include <imgui.h>
+
 namespace Hazel {
 
 	OrthographicCameraController::OrthographicCameraController(float aspectRatio, bool rotation)
@@ -15,41 +17,46 @@ namespace Hazel {
 	{
 		HZ_PROFILE_FUNCTION();
 
-		if (Input::IsKeyPressed(HZ_KEY_A))
-		{
-			m_CameraPosition.x -= cos(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
-			m_CameraPosition.y -= sin(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
-		}
-		else if (Input::IsKeyPressed(HZ_KEY_D))
-		{
-			m_CameraPosition.x += cos(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
-			m_CameraPosition.y += sin(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
-		}
+		ImGuiIO& io = ImGui::GetIO();
 
-		if (Input::IsKeyPressed(HZ_KEY_W))
+		if (!io.WantCaptureKeyboard)
 		{
-			m_CameraPosition.x += -sin(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
-			m_CameraPosition.y += cos(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
-		}
-		else if (Input::IsKeyPressed(HZ_KEY_S))
-		{
-			m_CameraPosition.x -= -sin(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
-			m_CameraPosition.y -= cos(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
-		}
+			if (Input::IsKeyPressed(HZ_KEY_A))
+			{
+				m_CameraPosition.x -= cos(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
+				m_CameraPosition.y -= sin(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
+			}
+			else if (Input::IsKeyPressed(HZ_KEY_D))
+			{
+				m_CameraPosition.x += cos(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
+				m_CameraPosition.y += sin(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
+			}
 
-		if (m_Rotation)
-		{
-			if (Input::IsKeyPressed(HZ_KEY_Q))
-				m_CameraRotation += m_CameraRotationSpeed * ts;
-			if (Input::IsKeyPressed(HZ_KEY_E))
-				m_CameraRotation -= m_CameraRotationSpeed * ts;
+			if (Input::IsKeyPressed(HZ_KEY_W))
+			{
+				m_CameraPosition.x += -sin(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
+				m_CameraPosition.y += cos(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
+			}
+			else if (Input::IsKeyPressed(HZ_KEY_S))
+			{
+				m_CameraPosition.x -= -sin(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
+				m_CameraPosition.y -= cos(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
+			}
 
-			if (m_CameraRotation > 180.0f)
-				m_CameraRotation -= 360.0f;
-			else if (m_CameraRotation <= -180.0f)
-				m_CameraRotation += 360.0f;
+			if (m_Rotation)
+			{
+				if (Input::IsKeyPressed(HZ_KEY_Q))
+					m_CameraRotation += m_CameraRotationSpeed * ts;
+				if (Input::IsKeyPressed(HZ_KEY_E))
+					m_CameraRotation -= m_CameraRotationSpeed * ts;
 
-			m_Camera.SetRotation(m_CameraRotation);
+				if (m_CameraRotation > 180.0f)
+					m_CameraRotation -= 360.0f;
+				else if (m_CameraRotation <= -180.0f)
+					m_CameraRotation += 360.0f;
+
+				m_Camera.SetRotation(m_CameraRotation);
+			}
 		}
 
 		m_Camera.SetPosition(m_CameraPosition);
