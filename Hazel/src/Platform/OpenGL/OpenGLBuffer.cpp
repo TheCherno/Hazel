15ab73262500679cb34b3b6f8efdfe25f01a3010
MrLever <nicolacognata@gmail.com>
2020-02-22 13:13:33 -0500

    Revert change to OpenGLBufferVertexBuffer.cpp

--- Hazel/src/Platform/OpenGL/OpenGLBuffer.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLBuffer.cpp
@@ -15,7 +15,7 @@ namespace Hazel {
 
 		glCreateBuffers(1, &m_RendererID);
 		glBindBuffer(GL_ARRAY_BUFFER, m_RendererID);
-		glBufferData(GL_ARRAY_BUFFER, size * sizeof(float), vertices, GL_STATIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, size, vertices, GL_STATIC_DRAW);
 	}
 
 	OpenGLVertexBuffer::~OpenGLVertexBuffer()
