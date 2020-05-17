7fa595072c32abf1770b82954cfcc92519006b3a
ilikenoodles2 <tdelapena20@gmail.com>
2020-05-17 05:08:20 -0500

    Imgui handling mouse and keyboards events (#219)

--- Hazel/src/Hazel/Core/Application.cpp
+++ Hazel/src/Hazel/Core/Application.cpp
@@ -61,9 +61,9 @@ namespace Hazel {
 
 		for (auto it = m_LayerStack.rbegin(); it != m_LayerStack.rend(); ++it)
 		{
-			(*it)->OnEvent(e);
-			if (e.Handled)
+			if (e.Handled) 
 				break;
+			(*it)->OnEvent(e);
 		}
 	}
 
--- Hazel/src/Hazel/ImGui/ImGuiLayer.cpp
+++ Hazel/src/Hazel/ImGui/ImGuiLayer.cpp
@@ -61,6 +61,13 @@ namespace Hazel {
 		ImGui_ImplGlfw_Shutdown();
 		ImGui::DestroyContext();
 	}
+
+	void ImGuiLayer::OnEvent(Event& e)
+	{
+		ImGuiIO& io = ImGui::GetIO();
+		e.Handled |= e.IsInCategory(EventCategoryMouse) & io.WantCaptureMouse;
+		e.Handled |= e.IsInCategory(EventCategoryKeyboard) & io.WantCaptureKeyboard;
+	}
 	
 	void ImGuiLayer::Begin()
 	{
