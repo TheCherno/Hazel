349eca3bba7a15c135e64da623e1a0e80aefa6bd
T4ouf <thomasvonascheberg@hotmail.fr>
2020-06-09 21:23:45 +0200

    Managed non-letter keys for other layouts

--- Hazel/src/Platform/Windows/WindowsInput.cpp
+++ Hazel/src/Platform/Windows/WindowsInput.cpp
@@ -18,12 +18,27 @@ namespace Hazel {
 
 	KeyCode WindowsInput::GetCrossLayoutKeyImpl(KeyCode key) {
 
+		const char* keyName = glfwGetKeyName(int(key), glfwGetKeyScancode(int(key)));
+
 		//Check if the key is concerned by keyboards layout (letters)
 		if (key >= KeyCode::A && key <= KeyCode::Z) {
-			const std::string keyName = glfwGetKeyName(int(key), glfwGetKeyScancode(int(key)));
 			return Key(int(KeyCode::A) + (std::toupper(keyName[0]) - 'A')); //Way to ensure that no matter the code that Hazel uses, the keycode will be correct
 		}
+		//Digit
+		else if ((key >= KeyCode::D0 && key <= KeyCode::D9) || (key >= KeyCode::KP0 && key <= KeyCode::KP9)) {
+			return key; //We treat digit as they are (regardless of the layout...)
+		}
+		//Other keys (mostly punctuation)
+		else if (key > KeyCode::Space && key <= KeyCode::World2) {
 
+			//Safety check for eventual gaps in Key codes
+			if (keyName != NULL) { 
+				return Key(int(KeyCode::Space) + (std::toupper(keyName[0]) - ' '));
+			}
+
+		}
+		
+		//the other keys are independant of the layout (like 'Space' 'Shift' etc.)
 		return key;
 	}
 
