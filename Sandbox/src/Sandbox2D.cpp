8408b0fd43770935445f7d0a93a49f2ba88e4f60
Jordan Day <jordanday444@gmail.com>
2020-06-11 09:47:37 -0500

    Fix flipped ImGui image

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -148,7 +148,7 @@ void Sandbox2D::OnImGuiRender()
 		ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
 
 		uint32_t textureID = m_Framebuffer->GetColorAttachmentRendererID();
-		ImGui::Image((void*)textureID, ImVec2{ 1280, 720 });
+		ImGui::Image((void*)textureID, ImVec2{ 1280, 720 }, ImVec2{0, 1}, ImVec2{1, 0});
 		ImGui::End();
 
 		ImGui::End();
