d2557b00811651c6f0ffd332a547d3d9d4ff17cb
VMormoris <48360903+VMormoris@users.noreply.github.com>
2020-06-16 23:22:54 +0300

    Adding virtual destructor on Framebuffer (#270)
    * Adding more virtual Destructors
    
    * Small rearrangement at Input\'s header file

--- Hazel/src/Hazel/Core/Input.h
+++ Hazel/src/Hazel/Core/Input.h
@@ -11,6 +11,8 @@ namespace Hazel {
 	protected:
 		Input() = default;
 	public:
+		virtual ~Input() = default;
+
 		Input(const Input&) = delete;
 		Input& operator=(const Input&) = delete;
 
--- Hazel/src/Hazel/Renderer/GraphicsContext.h
+++ Hazel/src/Hazel/Renderer/GraphicsContext.h
@@ -5,6 +5,8 @@ namespace Hazel {
 	class GraphicsContext
 	{
 	public:
+		virtual ~GraphicsContext() = default;
+
 		virtual void Init() = 0;
 		virtual void SwapBuffers() = 0;
 
