b50681d34af865f91eac379fc9edd8766fc6a24f
TheCherno <y@nchernikov.com>
2020-06-26 12:15:20 +1000

    Refactored Input "situation"

--- Hazel/src/Hazel/Core/Input.h
+++ Hazel/src/Hazel/Core/Input.h
@@ -8,30 +8,12 @@ namespace Hazel {
 
 	class Input
 	{
-	protected:
-		Input() = default;
 	public:
-		virtual ~Input() = default;
+		static bool IsKeyPressed(KeyCode key);
 
-		Input(const Input&) = delete;
-		Input& operator=(const Input&) = delete;
-
-		static bool IsKeyPressed(KeyCode key) { return s_Instance->IsKeyPressedImpl(key); }
-
-		static bool IsMouseButtonPressed(MouseCode button) { return s_Instance->IsMouseButtonPressedImpl(button); }
-		static std::pair<float, float> GetMousePosition() { return s_Instance->GetMousePositionImpl(); }
-		static float GetMouseX() { return s_Instance->GetMouseXImpl(); }
-		static float GetMouseY() { return s_Instance->GetMouseYImpl(); }
-
-		static Scope<Input> Create();
-	protected:
-		virtual bool IsKeyPressedImpl(KeyCode key) = 0;
-
-		virtual bool IsMouseButtonPressedImpl(MouseCode button) = 0;
-		virtual std::pair<float, float> GetMousePositionImpl() = 0;
-		virtual float GetMouseXImpl() = 0;
-		virtual float GetMouseYImpl() = 0;
-	private:
-		static Scope<Input> s_Instance;
+		static bool IsMouseButtonPressed(MouseCode button);
+		static std::pair<float, float> GetMousePosition();
+		static float GetMouseX();
+		static float GetMouseY();
 	};
 }
