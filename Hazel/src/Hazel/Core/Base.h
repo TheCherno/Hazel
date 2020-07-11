a6e1ead0cb65b09d3606dc577c39ee73091125da
#Momo <40578274+MomoDeve@users.noreply.github.com>
2020-07-11 12:24:52 +0300

    replaced std::bind with lambda in HZ_BIND_EVENT_FN macro
    using std::bind in modern C++ is bad practice, as lambdas can do the same and much more

--- Hazel/src/Hazel/Core/Base.h
+++ Hazel/src/Hazel/Core/Base.h
@@ -67,7 +67,7 @@
 
 #define BIT(x) (1 << x)
 
-#define HZ_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+#define HZ_BIND_EVENT_FN(fn) [this](auto&&... args) { return this->fn(args...); }
 
 namespace Hazel {
 
