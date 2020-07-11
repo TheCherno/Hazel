f3904b03497da33d733a7ad0e570a0779124bf5c
Sasha.potapov2002 <Sasha.potapov2002@yandex.ru>
2020-07-11 14:01:30 +0300

    improved HZ_BIND_EVENT_FN, now forwarding args and correctly deduce return type

--- Hazel/src/Hazel/Core/Base.h
+++ Hazel/src/Hazel/Core/Base.h
@@ -67,7 +67,7 @@
 
 #define BIT(x) (1 << x)
 
-#define HZ_BIND_EVENT_FN(fn) [this](auto&&... args) { return this->fn(args...); }
+#define HZ_BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
 
 namespace Hazel {
 
