3af25fde869fb14c8da95e332bba7332b8bff1e3
studiob56 <59216066+studiob56@users.noreply.github.com>
2020-07-25 21:22:54 +0100

    Fixed memory leak and malformed JSON in Instrumentor (#283)
    If a session is begun using HZ_PROFILE_BEGIN_SESSION(...) but HZ_PROFILE_END_SESSION() is not called before the program ends then the heap allocated InstrumentationSession is not deleted and the footer is not added to the file which creates malformed JSON.

--- Hazel/src/Hazel/Debug/Instrumentor.h
+++ Hazel/src/Hazel/Debug/Instrumentor.h
@@ -27,15 +27,9 @@ namespace Hazel {
 
 	class Instrumentor
 	{
-	private:
-		std::mutex m_Mutex;
-		InstrumentationSession* m_CurrentSession;
-		std::ofstream m_OutputStream;
 	public:
-		Instrumentor()
-			: m_CurrentSession(nullptr)
-		{
-		}
+		Instrumentor(const Instrumentor&) = delete;
+		Instrumentor(Instrumentor&&) = delete;
 
 		void BeginSession(const std::string& name, const std::string& filepath = "results.json")
 		{
@@ -102,8 +96,16 @@ namespace Hazel {
 			static Instrumentor instance;
 			return instance;
 		}
-
 	private:
+		Instrumentor()
+			: m_CurrentSession(nullptr)
+		{
+		}
+
+		~Instrumentor()
+		{
+			EndSession();
+		}		
 
 		void WriteHeader()
 		{
@@ -129,7 +131,10 @@ namespace Hazel {
 				m_CurrentSession = nullptr;
 			}
 		}
-
+	private:
+		std::mutex m_Mutex;
+		InstrumentationSession* m_CurrentSession;
+		std::ofstream m_OutputStream;
 	};
 
 	class InstrumentationTimer
