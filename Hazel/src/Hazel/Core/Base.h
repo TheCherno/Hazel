9f8f196a2d244f71e8e759c18ae398028839ed5f
#Momo <40578274+MomoDeve@users.noreply.github.com>
2020-07-25 23:26:21 +0300

    replaced std::bind in HZ_BIND_EVENT_FN macro (#277)

--- Hazel/src/Hazel/Core/Base.h
+++ Hazel/src/Hazel/Core/Base.h
@@ -68,7 +68,7 @@
 
 #define BIT(x) (1 << x)
 
-#define HZ_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+#define HZ_BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
 
 namespace Hazel {
 
