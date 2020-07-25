120df0feaf5a4d3c9c17842f3b54945694c77685
Gil Balsiger <balsigergil@users.noreply.github.com>
2020-07-25 22:33:47 +0200

    Fixed invalid entt::entity equal entt::null, which is not equal to 0 (#285)
    See https://github.com/skypjack/entt/wiki/Crash-Course:-entity-component-system#null-entity for more details.

--- Hazel/src/Hazel/Scene/Entity.h
+++ Hazel/src/Hazel/Scene/Entity.h
@@ -40,9 +40,9 @@ namespace Hazel {
 			m_Scene->m_Registry.remove<T>(m_EntityHandle);
 		}
 
-		operator bool() const { return m_EntityHandle != 0; }
+		operator bool() const { return m_EntityHandle != entt::null; }
 	private:
-		entt::entity m_EntityHandle{ 0 };
+		entt::entity m_EntityHandle{ entt::null };
 		Scene* m_Scene = nullptr;
 	};
 
