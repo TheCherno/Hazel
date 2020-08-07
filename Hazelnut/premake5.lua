b8620c740cc150e066eb2d8d47abb2af34c2e560
LaurensScheldeman <laurens.scheldeman@gmail.com>
2020-08-07 18:32:40 +0200

    split premake file for different projects

---
+++ Hazelnut/premake5.lua
@@ -0,0 +1,46 @@
+project "Hazelnut"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp"
+	}
+
+	includedirs
+	{
+		"../Hazel/vendor/spdlog/include",
+		"../Hazel/src",
+		"../Hazel/vendor",
+		"../%{IncludeDir.glm}",
+		"../%{IncludeDir.entt}"
+	}
+
+	links
+	{
+		"Hazel"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		runtime "Release"
+		optimize "on"
