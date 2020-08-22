a6064c50237ff7a0739c823852d4fbd18617442a
Ujjwal Raut <ujjwalraut.14@gmail.com>
2020-08-23 04:35:48 +0530

    Added #define NOMINMAX to pch file (#316)

--- Hazel/src/hzpch.h
+++ Hazel/src/hzpch.h
@@ -1,5 +1,14 @@
 #pragma once
 
+#include "Hazel/Core/PlatformDetection.h"
+
+#ifdef HZ_PLATFORM_WINDOWS
+	#ifndef NOMINMAX
+		// See github.com/skypjack/entt/wiki/Frequently-Asked-Questions#warning-c4003-the-min-the-max-and-the-macro
+		#define NOMINMAX
+	#endif
+#endif
+
 #include <iostream>
 #include <memory>
 #include <utility>
