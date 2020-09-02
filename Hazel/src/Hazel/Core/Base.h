20083670bb968c5b45ec6f55c7a8e006cf7a8eea
Jonathan Jopp <jonathanjopp@gmail.com>
2020-09-02 21:41:18 +0200

    Removed decltype(auto) annotation in HZ_BIND_EVENT_FN lambda

--- Hazel/src/Hazel/Core/Base.h
+++ Hazel/src/Hazel/Core/Base.h
@@ -29,7 +29,7 @@
 
 #define BIT(x) (1 << x)
 
-#define HZ_BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
+#define HZ_BIND_EVENT_FN(fn) [this](auto&&... args) { return this->fn(std::forward<decltype(args)>(args)...); }
 
 namespace Hazel {
 
