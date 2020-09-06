fddf214b1e40781f7b094b739977f76a2c70cc3a
Cael Rasmussen <53895123+caelwithcats@users.noreply.github.com>
2020-09-06 14:47:42 +1000

    Remove easter egg from scripts/Linux-GenProjects.sh
    Co-authored-by: lovely_santa <laurens.scheldeman@gmail.com>

--- scripts/Linux-GenProjects.sh
+++ scripts/Linux-GenProjects.sh
@@ -1,24 +1,3 @@
-<<'###COMMENT'
-linux users are welcome too
-
-         _nnnn_        
-        dGGGGMMb       
-       @p~qp~~qMb      
-       M|@||@) M|      
-       @,----.JM|      
-      JS^\__/  qKL     
-     dZP        qKRb   
-    dZP          qKKb  
-   fZP            SMMb 
-   HZM            MMMM 
-   FqM            MMMM 
- __| ".        |\dS"qML
- |    `.       | `' \Zq
-_)      \.___.,|     .'
-\____   )MMMMMP|   .'  
-     `-'       `--' hjm
-###COMMENT
-
 dp0=$(dirname $(readlink -f $0))
 cd "$dp0/.."
 ./vendor/premake/bin/premake5-linux gmake2
