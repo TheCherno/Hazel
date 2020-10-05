b538e7cee4ad18f220b45658053d4e0c96c14ebf
TigerCipher <67520209+TigerCipher@users.noreply.github.com>
2020-10-05 14:50:18 -0500

    Allow assert macro to take no arguments and fix Log.h include shenanigans (#306)

---
+++ Hazel/src/Hazel/Core/Assert.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "Hazel/Core/Base.h"
+#include "Hazel/Core/Log.h"
+
+#ifdef HZ_ENABLE_ASSERTS
+
+	namespace Hazel::Assert
+	{
+		// Returns the simple file name rather than full path as suggested by LovelySanta
+		constexpr const char* CurrentFileName(const char* path) {
+				const char* file = path;
+				while (*path)
+				{
+					if (*path == '/' || *path == '\\')
+						file = ++path;
+					else
+						path++;
+				}
+				return file;
+			}
+	}
+
+	// Alteratively we could use the same "default" message for both "WITH_MSG" and "NO_MSG" and
+	// provide support for custom formatting by concatenating the formatting string instead of having the format inside the default message
+	#define HZ_INTERNAL_ASSERT_IMPL(type, check, msg, ...) { if(!(check)) { HZ##type##ERROR(msg, __VA_ARGS__); HZ_DEBUGBREAK(); } }
+	#define HZ_INTERNAL_ASSERT_WITH_MSG(type, check, ...) HZ_INTERNAL_ASSERT_IMPL(type, check, "Assertion failed: {0}", __VA_ARGS__)
+	#define HZ_INTERNAL_ASSERT_NO_MSG(type, check) HZ_INTERNAL_ASSERT_IMPL(type, check, "Assertion '{0}' failed at {1}:{2}", HZ_STRINGIFY_MACRO(check), ::Hazel::Assert::CurrentFileName(__FILE__), __LINE__)
+
+	#define HZ_INTERNAL_ASSERT_GET_MACRO_NAME(arg1, arg2, macro, ...) macro
+	#define HZ_INTERNAL_ASSERT_GET_MACRO(...) HZ_EXPAND_MACRO( HZ_INTERNAL_ASSERT_GET_MACRO_NAME(__VA_ARGS__, HZ_INTERNAL_ASSERT_WITH_MSG, HZ_INTERNAL_ASSERT_NO_MSG) )
+
+	// Currently accepts at least the condition and one additional parameter (the message) being optional
+	#define HZ_ASSERT(...) HZ_EXPAND_MACRO( HZ_INTERNAL_ASSERT_GET_MACRO(__VA_ARGS__)(_, __VA_ARGS__) )
+	#define HZ_CORE_ASSERT(...) HZ_EXPAND_MACRO( HZ_INTERNAL_ASSERT_GET_MACRO(__VA_ARGS__)(_CORE_, __VA_ARGS__) )
+#else
+	#define HZ_ASSERT(...)
+	#define HZ_CORE_ASSERT(...)
+#endif
