0ee7476c85190c5e34f97aaf0aaae2736f37855e
Ben <bccooper1996@gmail.com>
2020-10-18 03:24:06 -0700

    Make FlushAndReset() not call EndScene() (#344)
    By introducing a function StartBatch(), renamed FlushAndReset() to NextBatch()

--- Hazel/src/Hazel/Renderer/Renderer2D.h
+++ Hazel/src/Hazel/Renderer/Renderer2D.h
@@ -44,8 +44,10 @@ namespace Hazel {
 		};
 		static void ResetStats();
 		static Statistics GetStats();
+
 	private:
-		static void FlushAndReset();
+		static void StartBatch();
+		static void NextBatch();
 	};
 
 }
