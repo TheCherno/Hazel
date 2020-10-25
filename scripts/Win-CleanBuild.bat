15e2345e4bfaf51e3c3e8461851e60fe861a728d
Ujjwal Raut <ujjwalraut.14@gmail.com>
2020-10-25 19:46:25 +0530

    Minor refactor

--- scripts/Win-CleanBuild.bat
+++ scripts/Win-CleanBuild.bat
@@ -1,8 +1,7 @@
 @echo off
 pushd %~dp0\..\
 RMDIR /S /Q .vs
-DEL *.sln 
-DEL /S /Q *.vcxproj *.vcxproj.filters *.vcxproj.user
+DEL /S /Q *.sln *.vcxproj *.vcxproj.filters *.vcxproj.user
 FOR /d /r . %%d IN ("bin") DO @IF EXIST "%%d" rd /s /q "%%d"
 FOR /d /r . %%d IN ("bin-int") DO @IF EXIST "%%d" rd /s /q "%%d"
 popd
