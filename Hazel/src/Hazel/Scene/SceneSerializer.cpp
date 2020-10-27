21f1747e9e8f1af757218a37548b94c36280896f
Sepehr Kalanaki <prowidgs@gmail.com>
2020-10-27 15:15:42 +0330

    Set vec4 and vec2's encoding style to Flow

--- Hazel/src/Hazel/Scene/SceneSerializer.cpp
+++ Hazel/src/Hazel/Scene/SceneSerializer.cpp
@@ -19,6 +19,7 @@ namespace YAML {
 			node.push_back(rhs.x);
 			node.push_back(rhs.y);
 			node.push_back(rhs.z);
+			node.SetStyle(EmitterStyle::Flow);
 			return node;
 		}
 
@@ -44,6 +45,7 @@ namespace YAML {
 			node.push_back(rhs.y);
 			node.push_back(rhs.z);
 			node.push_back(rhs.w);
+			node.SetStyle(EmitterStyle::Flow);
 			return node;
 		}
 
