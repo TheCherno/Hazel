6ad9f22b896d6329b912807ec6b2168310f5c979
Sepehr Kalanaki <prowidgs@gmail.com>
2020-10-27 18:49:36 +0330

    Now scene file is loaded by yaml-cpp

--- Hazel/src/Hazel/Scene/SceneSerializer.cpp
+++ Hazel/src/Hazel/Scene/SceneSerializer.cpp
@@ -181,11 +181,7 @@ namespace Hazel {
 
 	bool SceneSerializer::Deserialize(const std::string& filepath)
 	{
-		std::ifstream stream(filepath);
-		std::stringstream strStream;
-		strStream << stream.rdbuf();
-
-		YAML::Node data = YAML::Load(strStream.str());
+		YAML::Node data = YAML::LoadFile(filepath);
 		if (!data["Scene"])
 			return false;
 
