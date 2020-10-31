30d03d543570a12d9f3f842e9c556c8494c09763
unknown <advikkabra25@gmail.com>
2020-10-31 21:22:47 +0530

    Warn if component is already added

--- Hazelnut/src/Panels/SceneHierarchyPanel.cpp
+++ Hazelnut/src/Panels/SceneHierarchyPanel.cpp
@@ -221,23 +221,23 @@ namespace Hazel {
 
 		if (ImGui::BeginPopup("AddComponent"))
 		{
-			if (!m_SelectionContext.HasComponent<CameraComponent>())
-			{
 				if (ImGui::MenuItem("Camera"))
 				{
-					m_SelectionContext.AddComponent<CameraComponent>();
+					if (!m_SelectionContext.HasComponent<CameraComponent>())
+						m_SelectionContext.AddComponent<CameraComponent>();
+					else
+						HZ_CORE_WARN("This entity already has the Camera Component!");
 					ImGui::CloseCurrentPopup();
 				}
-			}
 
-			if (!m_SelectionContext.HasComponent<SpriteRendererComponent>())
-			{
 				if (ImGui::MenuItem("Sprite Renderer"))
 				{
-					m_SelectionContext.AddComponent<SpriteRendererComponent>();
+					if (!m_SelectionContext.HasComponent<SpriteRendererComponent>())
+						m_SelectionContext.AddComponent<SpriteRendererComponent>();
+					else
+						HZ_CORE_WARN("This entity already has the Sprite Renderer Component!");
 					ImGui::CloseCurrentPopup();
 				}
-			}
 
 			ImGui::EndPopup();
 		}
