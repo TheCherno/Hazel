bd860d041b41ad1e72031c767d869faebe858af3
Sepehr Kalanaki <prowidgs@gmail.com>
2020-11-03 04:29:43 +0330

    Use YAML::LoadFile to load scene files (#358)

--- Hazel/src/Hazel/Scene/SceneSerializer.cpp
+++ Hazel/src/Hazel/Scene/SceneSerializer.cpp
@@ -19,6 +19,7 @@ namespace YAML {
 			node.push_back(rhs.x);
 			node.push_back(rhs.y);
 			node.push_back(rhs.z);
+			node.SetStyle(EmitterStyle::Flow);
 			return node;
 		}
 
@@ -44,6 +45,7 @@ namespace YAML {
 			node.push_back(rhs.y);
 			node.push_back(rhs.z);
 			node.push_back(rhs.w);
+			node.SetStyle(EmitterStyle::Flow);
 			return node;
 		}
 
@@ -179,11 +181,7 @@ namespace Hazel {
 
 	bool SceneSerializer::Deserialize(const std::string& filepath)
 	{
-		std::ifstream stream(filepath);
-		std::stringstream strStream;
-		strStream << stream.rdbuf();
-
-		YAML::Node data = YAML::Load(strStream.str());
+		YAML::Node data = YAML::LoadFile(filepath);
 		if (!data["Scene"])
 			return false;
 
