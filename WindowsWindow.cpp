6b26b059c9a30dd02e82d269bcb61bfefa403fb6
standtall007 <davidm@nutcrackz.com>
2020-11-22 04:53:24 +0100

    Changed scale factor to use yscale instead of xscale.
    Changed scale factor to use yscale instead of xscale.

---
+++ WindowsWindow.cpp
@@ -0,0 +1,228 @@
+#include "hzpch.h"
+#include "Platform/Windows/WindowsWindow.h"
+
+#include "Hazel/Core/Input.h"
+
+#include "Hazel/Events/ApplicationEvent.h"
+#include "Hazel/Events/MouseEvent.h"
+#include "Hazel/Events/KeyEvent.h"
+
+#include "Hazel/Renderer/Renderer.h"
+
+#include "Platform/OpenGL/OpenGLContext.h"
+
+namespace Hazel {
+	
+	float Window::s_HighDPIScaleFactor = 1.0f;
+<<<<<<< HEAD
+
+=======
+	
+>>>>>>> fa9107a0f02a04e465adf0c3d6feb626770fee7f
+	static uint8_t s_GLFWWindowCount = 0;
+
+	static void GLFWErrorCallback(int error, const char* description)
+	{
+		HZ_CORE_ERROR("GLFW Error ({0}): {1}", error, description);
+	}
+
+	WindowsWindow::WindowsWindow(const WindowProps& props)
+	{
+		HZ_PROFILE_FUNCTION();
+
+		Init(props);
+	}
+
+	WindowsWindow::~WindowsWindow()
+	{
+		HZ_PROFILE_FUNCTION();
+
+		Shutdown();
+	}
+
+	void WindowsWindow::Init(const WindowProps& props)
+	{
+		HZ_PROFILE_FUNCTION();
+
+		m_Data.Title = props.Title;
+		m_Data.Width = props.Width;
+		m_Data.Height = props.Height;
+
+		HZ_CORE_INFO("Creating window {0} ({1}, {2})", props.Title, props.Width, props.Height);
+
+		if (s_GLFWWindowCount == 0)
+		{
+			HZ_PROFILE_SCOPE("glfwInit");
+			int success = glfwInit();
+			HZ_CORE_ASSERT(success, "Could not initialize GLFW!");
+			glfwSetErrorCallback(GLFWErrorCallback);
+		}
+
+		{
+			HZ_PROFILE_SCOPE("glfwCreateWindow");
+<<<<<<< HEAD
+
+=======
+			
+>>>>>>> fa9107a0f02a04e465adf0c3d6feb626770fee7f
+			GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+			float xscale, yscale;
+			glfwGetMonitorContentScale(monitor, &xscale, &yscale);
+			
+			if (xscale > 1.0f || yscale > 1.0f)
+			{
+<<<<<<< HEAD
+				s_HighDPIScaleFactor = yscale;
+				glfwWindowHint(GLFW_SCALE_TO_MONITOR, GLFW_TRUE);
+			}
+
+=======
+				s_HighDPIScaleFactor = xscale;
+				glfwWindowHint(GLFW_SCALE_TO_MONITOR, GLFW_TRUE);
+			}
+			
+>>>>>>> fa9107a0f02a04e465adf0c3d6feb626770fee7f
+		#if defined(HZ_DEBUG)
+			if (Renderer::GetAPI() == RendererAPI::API::OpenGL)
+				glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
+		#endif
+			m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
+			++s_GLFWWindowCount;
+		}
+
+		m_Context = GraphicsContext::Create(m_Window);
+		m_Context->Init();
+
+		glfwSetWindowUserPointer(m_Window, &m_Data);
+		SetVSync(true);
+
+		// Set GLFW callbacks
+		glfwSetWindowSizeCallback(m_Window, [](GLFWwindow* window, int width, int height)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+			data.Width = width;
+			data.Height = height;
+
+			WindowResizeEvent event(width, height);
+			data.EventCallback(event);
+		});
+
+		glfwSetWindowCloseCallback(m_Window, [](GLFWwindow* window)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+			WindowCloseEvent event;
+			data.EventCallback(event);
+		});
+
+		glfwSetKeyCallback(m_Window, [](GLFWwindow* window, int key, int scancode, int action, int mods)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+			switch (action)
+			{
+				case GLFW_PRESS:
+				{
+					KeyPressedEvent event(key, 0);
+					data.EventCallback(event);
+					break;
+				}
+				case GLFW_RELEASE:
+				{
+					KeyReleasedEvent event(key);
+					data.EventCallback(event);
+					break;
+				}
+				case GLFW_REPEAT:
+				{
+					KeyPressedEvent event(key, 1);
+					data.EventCallback(event);
+					break;
+				}
+			}
+		});
+
+		glfwSetCharCallback(m_Window, [](GLFWwindow* window, unsigned int keycode)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+			KeyTypedEvent event(keycode);
+			data.EventCallback(event);
+		});
+
+		glfwSetMouseButtonCallback(m_Window, [](GLFWwindow* window, int button, int action, int mods)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+			switch (action)
+			{
+				case GLFW_PRESS:
+				{
+					MouseButtonPressedEvent event(button);
+					data.EventCallback(event);
+					break;
+				}
+				case GLFW_RELEASE:
+				{
+					MouseButtonReleasedEvent event(button);
+					data.EventCallback(event);
+					break;
+				}
+			}
+		});
+
+		glfwSetScrollCallback(m_Window, [](GLFWwindow* window, double xOffset, double yOffset)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+			MouseScrolledEvent event((float)xOffset, (float)yOffset);
+			data.EventCallback(event);
+		});
+
+		glfwSetCursorPosCallback(m_Window, [](GLFWwindow* window, double xPos, double yPos)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+			MouseMovedEvent event((float)xPos, (float)yPos);
+			data.EventCallback(event);
+		});
+	}
+
+	void WindowsWindow::Shutdown()
+	{
+		HZ_PROFILE_FUNCTION();
+
+		glfwDestroyWindow(m_Window);
+		--s_GLFWWindowCount;
+
+		if (s_GLFWWindowCount == 0)
+		{
+			glfwTerminate();
+		}
+	}
+
+	void WindowsWindow::OnUpdate()
+	{
+		HZ_PROFILE_FUNCTION();
+
+		glfwPollEvents();
+		m_Context->SwapBuffers();
+	}
+
+	void WindowsWindow::SetVSync(bool enabled)
+	{
+		HZ_PROFILE_FUNCTION();
+
+		if (enabled)
+			glfwSwapInterval(1);
+		else
+			glfwSwapInterval(0);
+
+		m_Data.VSync = enabled;
+	}
+
+	bool WindowsWindow::IsVSync() const
+	{
+		return m_Data.VSync;
+	}
+
+}
