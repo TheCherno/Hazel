1cc6accb090184e661d07917c9a2fb7b6161b581
Ujjwal Raut <ujjwalraut.14@gmail.com>
2020-12-04 20:24:48 +0530

    Fixed Function Declaration

--- Hazel/src/Hazel/Math/Math.h
+++ Hazel/src/Hazel/Math/Math.h
@@ -3,7 +3,5 @@
 #include <glm/glm.hpp>
 
 namespace Hazel::Math {
-
-	bool DecomposeTransform(const glm::mat4& transform, glm::vec3& outTranslation, glm::vec3& outRotation, glm::vec3& outScale);
-
+	bool DecomposeTransform(const glm::mat4& transform, glm::vec3& translation, glm::vec3& rotation, glm::vec3& scale);
 }
