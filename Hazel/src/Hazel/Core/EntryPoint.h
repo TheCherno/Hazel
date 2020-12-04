a2495efe348565093cf6233a79ab061b1aef1722
Ujjwal Raut <ujjwalraut.14@gmail.com>
2020-12-05 00:27:12 +0530

    Add code for launching with nVidia Card by default

--- Hazel/src/Hazel/Core/EntryPoint.h
+++ Hazel/src/Hazel/Core/EntryPoint.h
@@ -6,6 +6,10 @@
 
 extern Hazel::Application* Hazel::CreateApplication(ApplicationCommandLineArgs args);
 
+extern "C" {
+	__declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+}
+
 int main(int argc, char** argv)
 {
 	Hazel::Log::Init();
