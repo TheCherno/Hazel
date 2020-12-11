d819e2897bf7c44167d8cc6db9b8b36351519731
Ujjwal Raut <ujjwalraut.14@gmail.com>
2020-12-11 20:47:13 +0530

    Ref now get's created using CreateRef() in Log class

--- Hazel/src/Hazel/Core/Log.cpp
+++ Hazel/src/Hazel/Core/Log.cpp
@@ -5,7 +5,6 @@
 #include <spdlog/sinks/basic_file_sink.h>
 
 namespace Hazel {
-
 	Ref<spdlog::logger> Log::s_CoreLogger;
 	Ref<spdlog::logger> Log::s_ClientLogger;
 
@@ -18,16 +17,14 @@ namespace Hazel {
 		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
 		logSinks[1]->set_pattern("[%T] [%l] %n: %v");
 
-		s_CoreLogger = std::make_shared<spdlog::logger>("HAZEL", begin(logSinks), end(logSinks));
+		s_CoreLogger = CreateRef<spdlog::logger>("HAZEL", begin(logSinks), end(logSinks));
 		spdlog::register_logger(s_CoreLogger);
 		s_CoreLogger->set_level(spdlog::level::trace);
 		s_CoreLogger->flush_on(spdlog::level::trace);
 
-		s_ClientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+		s_ClientLogger = CreateRef<spdlog::logger>("APP", begin(logSinks), end(logSinks));
 		spdlog::register_logger(s_ClientLogger);
 		s_ClientLogger->set_level(spdlog::level::trace);
 		s_ClientLogger->flush_on(spdlog::level::trace);
 	}
-
 }
-
