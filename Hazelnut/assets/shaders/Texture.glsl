30fe1c57592c6fe2844c9f18f336fb2caf51a286
Sausty <36990678+Sausty@users.noreply.github.com>
2020-12-11 14:27:21 +0100

    Fixed Z-Fighting bug in Texture Shader (#362)

--- Hazelnut/assets/shaders/Texture.glsl
+++ Hazelnut/assets/shaders/Texture.glsl
@@ -1,7 +1,7 @@
 // Basic Texture Shader
 
 #type vertex
-#version 330 core
+#version 450
 
 layout(location = 0) in vec3 a_Position;
 layout(location = 1) in vec4 a_Color;
@@ -13,7 +13,7 @@ uniform mat4 u_ViewProjection;
 
 out vec4 v_Color;
 out vec2 v_TexCoord;
-out float v_TexIndex;
+out flat float v_TexIndex;
 out float v_TilingFactor;
 
 void main()
@@ -26,13 +26,13 @@ void main()
 }
 
 #type fragment
-#version 330 core
+#version 450
 
 layout(location = 0) out vec4 color;
 
 in vec4 v_Color;
 in vec2 v_TexCoord;
-in float v_TexIndex;
+in flat float v_TexIndex;
 in float v_TilingFactor;
 
 uniform sampler2D u_Textures[32];
