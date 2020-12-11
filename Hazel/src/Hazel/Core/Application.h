a8a656f42b9ca411a414b4584b3b6d69c1dafb1a
Ridhwan <fahimfuad.dev@gmail.com>
2020-12-11 19:13:28 +0600

    Changed std::unique_ptr to Scope in Application.h (#376)

--- Hazel/src/Hazel/Core/Application.h
+++ Hazel/src/Hazel/Core/Application.h
@@ -38,7 +38,7 @@ namespace Hazel {
 		bool OnWindowClose(WindowCloseEvent& e);
 		bool OnWindowResize(WindowResizeEvent& e);
 	private:
-		std::unique_ptr<Window> m_Window;
+		Scope<Window> m_Window;
 		ImGuiLayer* m_ImGuiLayer;
 		bool m_Running = true;
 		bool m_Minimized = false;
