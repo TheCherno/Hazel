ad7fe2ea7d8819697644e3a8f7977cbed615a294
Ujjwal Raut <ujjwalraut.14@gmail.com>
2021-01-02 20:13:44 +0530

    Added AMD support

--- Hazelnut/src/HazelnutApp.cpp
+++ Hazelnut/src/HazelnutApp.cpp
@@ -7,6 +7,7 @@ namespace Hazel {
 	#ifdef HZ_PLATFORM_WINDOWS
 	extern "C" {
 		__declspec(dllexport) uint32_t NvOptimusEnablement = 0x00000001;
+		__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
 	}
 	#endif
 
