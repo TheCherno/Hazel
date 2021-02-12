6b272602c4c0fa14e9546d172dbd900064476433
Ujjwal Raut <ujjwalraut.14@gmail.com>
2021-02-12 20:19:51 +0530

    Fixed a rebase error

--- Hazel/src/Hazel/Core/EntryPoint.h
+++ Hazel/src/Hazel/Core/EntryPoint.h
@@ -6,18 +6,8 @@
 
 extern Hazel::Application* Hazel::CreateApplication(ApplicationCommandLineArgs args);
 
-<<<<<<< HEAD
-extern "C" {
-	__declspec(dllexport) uint32_t NvOptimusEnablement = 0x00000001;
-}
-
-extern "C" {
-	__declspec(dllexport) uint32_t NvOptimusEnablement = 0x00000001;
-}
-
 int main(int argc, char** argv)
 {
->>>>>>> 9ee977d... Updated as per @lovely_santa 's suggestion
 	Hazel::Log::Init();
 
 	HZ_PROFILE_BEGIN_SESSION("Startup", "HazelProfile-Startup.json");
