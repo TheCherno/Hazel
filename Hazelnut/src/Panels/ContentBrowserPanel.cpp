c184dd2ecd5d87cfeb3b521c7c88e9b85f7dd7a9
TheCherno <y@nchernikov.com>
2021-07-07 22:06:49 +1000

    Content Browser panel thumbnail layout

--- Hazelnut/src/Panels/ContentBrowserPanel.cpp
+++ Hazelnut/src/Panels/ContentBrowserPanel.cpp
@@ -11,6 +11,8 @@ namespace Hazel {
 	ContentBrowserPanel::ContentBrowserPanel()
 		: m_CurrentDirectory(s_AssetPath)
 	{
+		m_DirectoryIcon = Texture2D::Create("Resources/Icons/ContentBrowser/DirectoryIcon.png");
+		m_FileIcon = Texture2D::Create("Resources/Icons/ContentBrowser/FileIcon.png");
 	}
 
 	void ContentBrowserPanel::OnImGuiRender()
@@ -25,26 +27,43 @@ namespace Hazel {
 			}
 		}
 
+		static float padding = 16.0f;
+		static float thumbnailSize = 128.0f;
+		float cellSize = thumbnailSize + padding;
+
+		float panelWidth = ImGui::GetContentRegionAvail().x;
+		int columnCount = (int)(panelWidth / cellSize);
+		if (columnCount < 1)
+			columnCount = 1;
+
+		ImGui::Columns(columnCount, 0, false);
+
 		for (auto& directoryEntry : std::filesystem::directory_iterator(m_CurrentDirectory))
 		{
 			const auto& path = directoryEntry.path();
 			auto relativePath = std::filesystem::relative(path, s_AssetPath);
 			std::string filenameString = relativePath.filename().string();
-			if (directoryEntry.is_directory())
+			
+			Ref<Texture2D> icon = directoryEntry.is_directory() ? m_DirectoryIcon : m_FileIcon;
+			ImGui::ImageButton((ImTextureID)icon->GetRendererID(), { thumbnailSize, thumbnailSize }, { 0, 1 }, { 1, 0 });
+			if (ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left))
 			{
-				if (ImGui::Button(filenameString.c_str()))
-				{
+				if (directoryEntry.is_directory())
 					m_CurrentDirectory /= path.filename();
-				}
+
 			}
-			else
-			{
-				if (ImGui::Button(filenameString.c_str()))
-				{
-				}
-			}
+			ImGui::TextWrapped(filenameString.c_str());
+
+			ImGui::NextColumn();
+
 		}
 
+		ImGui::Columns(1);
+
+		ImGui::SliderFloat("Thumbnail Size", &thumbnailSize, 16, 512);
+		ImGui::SliderFloat("Padding", &padding, 0, 32);
+
+		// TODO: status bar
 		ImGui::End();
 	}
 
