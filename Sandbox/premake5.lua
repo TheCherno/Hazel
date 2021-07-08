d9552e2901b9d944165545c3e2abf156c281b6ff
0x <jrfwaite@gmail.com>
2021-07-09 03:50:09 +1200

    Fixes for fresh checkout compiles and both Hazelnut and Sandbox apps run (#454)
    TheCherno/yaml-cpp#1 is still an issue...

--- Sandbox/premake5.lua
+++ Sandbox/premake5.lua
@@ -2,7 +2,7 @@ project "Sandbox"
 	kind "ConsoleApp"
 	language "C++"
 	cppdialect "C++17"
-	staticruntime "on"
+	staticruntime "off"
 
 	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
