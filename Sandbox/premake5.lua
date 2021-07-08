04ad1c897602f415e9ae51361ac180b20ad5a920
Ujjwal Raut <ujjwalraut.14@gmail.com>
2021-07-08 21:30:30 +0530

    Added post-build commands for copying Vulkan .dlls to output directory (#437)

--- Sandbox/premake5.lua
+++ Sandbox/premake5.lua
@@ -34,6 +34,11 @@ project "Sandbox"
 		defines "HZ_DEBUG"
 		runtime "Debug"
 		symbols "on"
+		
+		postbuildcommands
+		{
+			"{COPYDIR} \"%{LibraryDir.VulkanSDK_DebugDLL}\" \"%{cfg.targetdir}\""
+		}
 
 	filter "configurations:Release"
 		defines "HZ_RELEASE"
