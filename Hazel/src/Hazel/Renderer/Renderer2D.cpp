99936f6128dded42f56d4cf38d592c248ed35577
TheCherno <y@nchernikov.com>
2021-07-30 21:25:58 +1000

    Added texture drag/drop and tiling factor to Sprite Component

--- Hazel/src/Hazel/Renderer/Renderer2D.cpp
+++ Hazel/src/Hazel/Renderer/Renderer2D.cpp
@@ -329,7 +329,10 @@ namespace Hazel {
 
 	void Renderer2D::DrawSprite(const glm::mat4& transform, SpriteRendererComponent& src, int entityID)
 	{
-		DrawQuad(transform, src.Color, entityID);
+		if (src.Texture)
+			DrawQuad(transform, src.Texture, src.TilingFactor, src.Color, entityID);
+		else
+			DrawQuad(transform, src.Color, entityID);
 	}
 
 	void Renderer2D::ResetStats()
