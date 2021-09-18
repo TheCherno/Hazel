6240078655731abeae125978442599d984b8858c
Ujjwal Raut <ujjwalraut.14@gmail.com>
2021-09-18 12:13:15 +0530

    Fixed typo in Log.h file

--- Hazel/src/Hazel/Core/Log.h
+++ Hazel/src/Hazel/Core/Log.h
@@ -40,9 +40,9 @@ inline OStream& operator<<(OStream& os, const glm::mat<C, R, T, Q>& matrix)
 }
 
 template<typename OStream, typename T, glm::qualifier Q>
-inline OStream& operator<<(OStream& os, glm::qua<T, Q> quaternio)
+inline OStream& operator<<(OStream& os, glm::qua<T, Q> quaternion)
 {
-	return os << glm::to_string(quaternio);
+	return os << glm::to_string(quaternion);
 }
 
 // Core log macros
