de990d39d2aebdaa0a379270d1070d6d6f0a5a37
Somotaw <Alexander.Bleckert@gmail.com>
2021-09-28 02:46:12 +0200

    Updated submodules and fixed issues

--- Hazel/src/Hazel/ImGui/ImGuiLayer.cpp
+++ Hazel/src/Hazel/ImGui/ImGuiLayer.cpp
@@ -2,8 +2,8 @@
 #include "Hazel/ImGui/ImGuiLayer.h"
 
 #include <imgui.h>
-#include <examples/imgui_impl_glfw.h>
-#include <examples/imgui_impl_opengl3.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
 
 #include "Hazel/Core/Application.h"
 
