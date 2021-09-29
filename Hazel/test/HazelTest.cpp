a1cfe5d4bef98705c8892f5d145ca8766c4031cf
niklasweidenfeller <niklas.weidenfeller@gmx.de>
2021-09-29 23:59:49 +0200

    Add example LogTest

--- Hazel/test/HazelTest.cpp
+++ Hazel/test/HazelTest.cpp
@@ -1,11 +1,6 @@
 #include "hzpch.h"
 #include <gtest/gtest.h>
 
-TEST(HazelTest, dummyTest)
-{
-	EXPECT_TRUE(TRUE);
-}
-
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
