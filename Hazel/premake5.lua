00864b0d8c78290a20ae8887767fddf1849348bd
niklasweidenfeller <77716996+niklasweidenfeller@users.noreply.github.com>
2021-09-30 04:14:21 +0200

    Add unit test support (#2)
    * add googletest submodule
    
    * configure gtest
    
    * change gtest to fork #1
    
    * switch to gtest fork #2
    
    * change example test
    
    * Add example LogTest
    
    * setup Gitactions for building mit msvc and running unit tests
    
    * Add example LogTest
    
    * Add example LogTest
    
    * add cd scripts
    
    * cmd as default shell
    
    * cmd as default shell
    
    * change order of steps
    
    * remove yes/no validation for git actions
    
    * debugging ...
    
    * try vulkan stuff
    
    * another vulkan try
    
    * skip vulkan
    
    * change path
    
    * change path
    
    * finaly working?
    
    * debug
    
    * makefile hopefully working
    
    * finally....?
    
    * switch to gmake2
    
    * try make install
    
    * try with msbuild
    
    * one last try
    
    * move setupgitactions
    
    * move log test
    
    * run on pr
    
    * remove "dir" prints, dummy failing test
    
    * remove dummy failing test
    
    * test a failing test
    
    * remove failing test

--- Hazel/premake5.lua
+++ Hazel/premake5.lua
@@ -98,3 +98,120 @@ project "Hazel"
 			"%{Library.SPIRV_Cross_Release}",
 			"%{Library.SPIRV_Cross_GLSL_Release}"
 		}
+
+project "HazelTest"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "off"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "hzpch.h"
+	pchsource "src/hzpch.cpp"
+
+	excludes "src/Hazel/Core/EntryPoint.h"
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+		"test/**.h",
+		"test/**.cpp",
+		"vendor/stb_image/**.h",
+		"vendor/stb_image/**.cpp",
+		"vendor/glm/glm/**.hpp",
+		"vendor/glm/glm/**.inl",
+
+		"vendor/ImGuizmo/ImGuizmo.h",
+		"vendor/ImGuizmo/ImGuizmo.cpp"
+	}
+
+	defines
+	{
+		"_CRT_SECURE_NO_WARNINGS",
+		"GLFW_INCLUDE_NONE"
+	}
+
+	includedirs
+	{
+		"src",
+		"vendor/spdlog/include",
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.Glad}",
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.glm}",
+		"%{IncludeDir.stb_image}",
+		"%{IncludeDir.entt}",
+		"%{IncludeDir.yaml_cpp}",
+		"%{IncludeDir.ImGuizmo}",
+		"%{IncludeDir.VulkanSDK}",
+		"%{IncludeDir.GoogleTest}"
+	}
+
+	links
+	{
+		"GLFW",
+		"Glad",
+		"ImGui",
+		"yaml-cpp",
+		"opengl32.lib",
+		"GoogleTest"
+	}
+
+	filter "files:vendor/ImGuizmo/**.cpp"
+	flags { "NoPCH", "NoImportLib" }
+	linkoptions
+	{
+		"/NODEFAULTLIB:LIBCMTD",
+		"/NODEFAULTLIB:RuntimeLibrary"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines
+		{
+		}
+
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+		links
+		{
+			"%{Library.ShaderC_Debug}",
+			"%{Library.SPIRV_Cross_Debug}",
+			"%{Library.SPIRV_Cross_GLSL_Debug}"
+		}
+
+		postbuildcommands
+		{
+			"{COPYDIR} \"%{LibraryDir.VulkanSDK_DebugDLL}\" \"%{cfg.targetdir}\""
+		}
+
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+		links
+		{
+			"%{Library.ShaderC_Release}",
+			"%{Library.SPIRV_Cross_Release}",
+			"%{Library.SPIRV_Cross_GLSL_Release}"
+		}
+
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		runtime "Release"
+		optimize "on"
+
+		links
+		{
+			"%{Library.ShaderC_Release}",
+			"%{Library.SPIRV_Cross_Release}",
+			"%{Library.SPIRV_Cross_GLSL_Release}"
+		}
---
+++ SetupGitActions/Setup.bat
@@ -0,0 +1,3 @@
+@echo off
+python Setup.py
+PAUSE
