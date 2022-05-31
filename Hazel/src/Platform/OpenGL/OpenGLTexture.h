0dd7e0147cf53f8b27293748bb057574326a31db
Voxel25 <58271696+Voxel25@users.noreply.github.com>
2022-05-31 19:00:15 +0300

    Update the old texture comparison behavior
    Replaced unnecessary casting with GetRendererID

--- Hazel/src/Platform/OpenGL/OpenGLTexture.h
+++ Hazel/src/Platform/OpenGL/OpenGLTexture.h
@@ -25,7 +25,7 @@ namespace Hazel {
 
 		virtual bool operator==(const Texture& other) const override
 		{
-			return m_RendererID == ((OpenGLTexture2D&)other).m_RendererID;
+			return m_RendererID == other.GetRendererID();
 		}
 	private:
 		std::string m_Path;
