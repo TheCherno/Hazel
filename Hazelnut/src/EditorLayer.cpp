5f5d7f0787934aa702a6ad8c283f5aeec16a57af
griush <gerardriush@gmail.com>
2022-06-11 00:41:18 +0200

    Fixed crash when opening a new scene during a physics simulation

--- Hazelnut/src/EditorLayer.cpp
+++ Hazelnut/src/EditorLayer.cpp
@@ -531,6 +531,9 @@ namespace Hazel {
 
 	void EditorLayer::NewScene()
 	{
+		if (m_SceneState != SceneState::Edit)
+			OnSceneStop();
+
 		m_ActiveScene = CreateRef<Scene>();
 		m_ActiveScene->OnViewportResize((uint32_t)m_ViewportSize.x, (uint32_t)m_ViewportSize.y);
 		m_SceneHierarchyPanel.SetContext(m_ActiveScene);
