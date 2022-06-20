3701bbbcd8b23654bd0d0493c7e2f65c89d0a226
TheCherno <y@nchernikov.com>
2022-06-20 19:53:13 +1000

    Changed KeyPressedEvent repeat count to "isRepeat"
    - To address PR #142
    - Actual key repeat count falls onto caller

--- Hazelnut/src/EditorLayer.cpp
+++ Hazelnut/src/EditorLayer.cpp
@@ -422,7 +422,7 @@ namespace Hazel {
 	bool EditorLayer::OnKeyPressed(KeyPressedEvent& e)
 	{
 		// Shortcuts
-		if (e.GetRepeatCount() > 0)
+		if (e.IsRepeat())
 			return false;
 
 		bool control = Input::IsKeyPressed(Key::LeftControl) || Input::IsKeyPressed(Key::RightControl);
