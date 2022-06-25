dcc4f2aeb3623ffb68f0e776b1c9b065ca22f422
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2022-06-25 21:02:34 +1200

    Additional Scope and Ref Functions

--- Hazel/src/Hazel/Core/Base.h
+++ Hazel/src/Hazel/Core/Base.h
@@ -29,14 +29,29 @@ namespace Hazel {
 
 	template<typename T>
 	using Scope = std::unique_ptr<T>;
+	template<typename T>
+	constexpr Scope<T> GetScope(T& ptr)
+	{
+		return std::unique_ptr<T>(&ptr);
+	}
 	template<typename T, typename ... Args>
 	constexpr Scope<T> CreateScope(Args&& ... args)
 	{
 		return std::make_unique<T>(std::forward<Args>(args)...);
 	}
-
+	
 	template<typename T>
 	using Ref = std::shared_ptr<T>;
+	template<typename T>
+	constexpr Ref<T> GetRef(T& ptr)
+	{
+		return std::shared_ptr<T>(&ptr);
+	}
+	template<typename T1, typename T2>
+	constexpr Ref<T1> CastRef(Ref<T2>& ptr)
+	{
+		return std::static_pointer_cast<T1>(ptr);
+	}
 	template<typename T, typename ... Args>
 	constexpr Ref<T> CreateRef(Args&& ... args)
 	{
