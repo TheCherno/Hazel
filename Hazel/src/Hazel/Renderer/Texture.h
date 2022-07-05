44ea684ee812169679993502be3241e340fa11da
Sebanisu <peeceseven@gmail.com>
2022-07-05 00:37:28 -0400

    Fix not compiling after merge. Had duplicate GetPath
    delete duplicate GetPath()
    
    The example isn't really needed to pull request. Cherno has his own.

--- Hazel/src/Hazel/Renderer/Texture.h
+++ Hazel/src/Hazel/Renderer/Texture.h
@@ -14,7 +14,6 @@ namespace Hazel {
 		virtual uint32_t GetWidth() const = 0;
 		virtual uint32_t GetHeight() const = 0;
 		virtual uint32_t GetRendererID() const = 0;
-		virtual const std::string & GetPath() const = 0;
 
 		virtual const std::string& GetPath() const = 0;
 
