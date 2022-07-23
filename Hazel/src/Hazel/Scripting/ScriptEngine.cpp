3cf297073c3accaa1511886a55916dbee4ed2c30
GrisWoldDiablo <GrisWoldDiablo@users.noreply.github.com>
2022-07-23 15:40:00 -0400

    ScriptEngine unloading domain during shutdown.
    In order to properly unload a MonoDomain, a different one has to be set. Since we are doing a clean up then we have to set it to the root domain.

--- Hazel/src/Hazel/Scripting/ScriptEngine.cpp
+++ Hazel/src/Hazel/Scripting/ScriptEngine.cpp
@@ -162,12 +162,12 @@ namespace Hazel {
 
 	void ScriptEngine::ShutdownMono()
 	{
-		// NOTE(Yan): mono is a little confusing to shutdown, so maybe come back to this
-
-		// mono_domain_unload(s_Data->AppDomain);
+		mono_domain_set(mono_get_root_domain(), false);
+		
+		mono_domain_unload(s_Data->AppDomain);
 		s_Data->AppDomain = nullptr;
 
-		// mono_jit_cleanup(s_Data->RootDomain);
+		mono_jit_cleanup(s_Data->RootDomain);
 		s_Data->RootDomain = nullptr;
 	}
 
