92ca27aeb9bfd46ea1e6e61c8b188a800391a506
Rafael Fernandes <rflgf0@gmail.com>
2022-10-28 10:17:02 -0300

    Fixed wrong matrix multiplication order in OrthographicCamera

--- Hazel/src/Hazel/Renderer/OrthographicCamera.cpp
+++ Hazel/src/Hazel/Renderer/OrthographicCamera.cpp
@@ -25,8 +25,8 @@ namespace Hazel {
 	{
 		HZ_PROFILE_FUNCTION();
 
-		glm::mat4 transform = glm::translate(glm::mat4(1.0f), m_Position) *
-			glm::rotate(glm::mat4(1.0f), glm::radians(m_Rotation), glm::vec3(0, 0, 1));
+		glm::mat4 transform = glm::rotate(glm::mat4(1.0f), glm::radians(m_Rotation), glm::vec3(0, 0, 1)) *
+            glm::translate(glm::mat4(1.0f), m_Position);
 
 		m_ViewMatrix = glm::inverse(transform);
 		m_ViewProjectionMatrix = m_ProjectionMatrix * m_ViewMatrix;
