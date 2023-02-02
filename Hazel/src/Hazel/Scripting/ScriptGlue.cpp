ca85a22d8ddf896a169d91d1bb450519a29bcc7f
ataberkaslan <aslanataberk06@gmail.com>
2023-02-02 18:12:23 +0300

    Unused stuff cleared

--- Hazel/src/Hazel/Scripting/ScriptGlue.cpp
+++ Hazel/src/Hazel/Scripting/ScriptGlue.cpp
@@ -22,25 +22,6 @@ namespace Hazel {
 
 #define HZ_ADD_INTERNAL_CALL(Name) mono_add_internal_call("Hazel.InternalCalls::" #Name, Name)
 
-	static void NativeLog(MonoString* string, int parameter)
-	{
-		char* cStr = mono_string_to_utf8(string);
-		std::string str(cStr);
-		mono_free(cStr);
-		std::cout << str << ", " << parameter << std::endl;
-	}
-
-	static void NativeLog_Vector(glm::vec3* parameter, glm::vec3* outResult)
-	{
-		HZ_CORE_WARN("Value: {0}", *parameter);
-		*outResult = glm::normalize(*parameter);
-	}
-
-	static float NativeLog_VectorDot(glm::vec3* parameter)
-	{
-		HZ_CORE_WARN("Value: {0}", *parameter);
-		return glm::dot(*parameter, *parameter);
-	}
 
 	static MonoObject* GetScriptInstance(UUID entityID)
 	{
@@ -194,10 +175,6 @@ namespace Hazel {
 
 	void ScriptGlue::RegisterFunctions()
 	{
-		HZ_ADD_INTERNAL_CALL(NativeLog);
-		HZ_ADD_INTERNAL_CALL(NativeLog_Vector);
-		HZ_ADD_INTERNAL_CALL(NativeLog_VectorDot);
-
 		HZ_ADD_INTERNAL_CALL(GetScriptInstance);
 
 		HZ_ADD_INTERNAL_CALL(Entity_HasComponent);
