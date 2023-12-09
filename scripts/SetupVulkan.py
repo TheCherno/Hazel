65ffdb8e58fb7e8c51311f4210ab95f529f537da
soulwax <soulwax-95@protonmail.com>
2023-12-09 14:47:40 +0100

    Update Premake and Vulkan versions
    Bumped up the Premake version to "5.0.0-beta2" and the Vulkan version to "1.3.268.0" for integration with the latest features and improvements.
    
    Confirmed its working state.

--- scripts/SetupVulkan.py
+++ scripts/SetupVulkan.py
@@ -10,7 +10,7 @@ from urllib.request import urlopen
 
 class VulkanConfiguration:
     requiredVulkanVersion = "1.3."
-    installVulkanVersion = "1.3.216.0"
+    installVulkanVersion = "1.3.268.0"
     vulkanDirectory = "./Hazel/vendor/VulkanSDK"
 
     @classmethod
